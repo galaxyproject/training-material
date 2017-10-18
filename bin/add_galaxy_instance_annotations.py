7e633e42f94206e4a29e3c01892a42295569a513
Bérénice Batut <bebatut@gmail.com>
2017-10-18 16:38:22 +0200

    Fix extraction of tested galaxy instances

--- bin/add_galaxy_instance_annotations.py
+++ bin/add_galaxy_instance_annotations.py
@@ -3,141 +3,106 @@
 import os
 import argparse
 import yaml
-from bioblend import galaxy
 import requests
 import io
 import pandas as pd
 
-log_dir = "logs"
-
 
 def extract_public_galaxy_servers():
-    """ Extract a pandas Data frame with the public Galaxy servers """
+    """Extract a pandas Data frame with the public Galaxy servers"""
     r = requests.get('https://raw.githubusercontent.com/martenson/public-galaxy-servers/master/servers.csv')
     f = io.StringIO(r.text)
     public_galaxy_servers = pd.read_csv(f, sep=",")
     return public_galaxy_servers
 
 
-def test_server_connection(galaxy_url):
-    """ Test if the server is accessible and return the GalaxyInstance object """
-    gi = galaxy.GalaxyInstance(url=galaxy_url)
-    try:
-        gi.config.get_config()
-    except:
-        print("Can not connect to %s" % galaxy_url)
-        return None
-    return gi
+def extract_public_galaxy_servers_tools():
+    """Extract the tools from the public Galaxy servers using their API"""
+    servers = extract_public_galaxy_servers()
+    server_tools = {}
+    for index, server in servers.iterrows():
+        #print(server['name'])
+        if server['name'] != "Freiburg Galaxy":
+            continue
+        # request the tools via the API
+        url = '%s/api/tools' % server['url']
+        response = requests.get(url)
+        # check status
+        if response.status_code != requests.codes.ok:
+            #rej_servers[server['name']] = {
+            #    'url': server['url'],
+            #    'status': response.status_code}
+            continue
+        # check content
+        if response.text.find("</html>") != -1:
+            #rej_servers[server['name']] = {
+            #    'url': server['url'],
+            #    'status': "Bad JSON"}
+            continue
+        # extract the list of tools in this instance
+        found_tools = set()
+        for section in response.json():
+            if 'elems' in section:
+                for tool in section['elems']:
+                    found_tools.add('/'.join( tool['id'].split('/')[:4] ))
+        # save the server with its tools
+        server_tools[server['name']] = {
+            'url': server['url'],
+            'tools': found_tools
+        }
+    return server_tools
 
 
-def check_instance(gi, tool_filepath, log_file):
-    """ Check if a given tutorial can be run on a given Galaxy instance """
-    # get tools in the instance
-    try:
-        tools_list = gi.tools.get_tools()
-    except:
-        print("Can not geet tools from %s" % galaxy_url)
-        return None
-    # find tools needed for the tutorial
-    with open(tool_filepath) as f:
+def extract_tool_list(tool_filepath):
+    """Extract the list of tools for the tutorial"""
+    with open(tool_filepath, "r") as f:
         tool_yaml = yaml.safe_load(f)
-    tools_required = {}
+    tools = set()
     for tool in tool_yaml['tools']:
-        tools_required.setdefault(
-            tool['name'],
-            {'found': False, 'revisions': {}})
-        if 'revisions' in tool:
-            for revision in tool['revisions']:
-                tools_required[tool['name']]['revisions'].setdefault(revision, False)
-    # check if all tools are installed
-    for t in tools_list:
-        if 'tool_shed_repository' not in t:
-            continue
-        name = t['tool_shed_repository']['name']
-        revision = t['tool_shed_repository']['changeset_revision']
-        if name in tools_required:
-            tools_required[name]['found'] = True
-            if revision in tools_required[name]['revisions']:
-                tools_required[name]['revisions'][revision] = True
-    # write in log file what is installed and what is missing
-    tool_requirements_satisfied = True
-    with open(log_file, 'a') as f:
-        f.write("Tutorial: %s\n" % (tool_filepath))
-        for tool in tools_required:
-            f.write(" - %s: %s\n" % (tool, tools_required[tool]['found']))
-            tool_requirements_satisfied &= tools_required[tool]['found']
-            for revision in tools_required[tool]['revisions']:
-                f.write("  - %s: %s\n" % (revision, tools_required[tool]['revisions'][revision]))
-        f.write("\n")
-    return tool_requirements_satisfied
+        tools.add('toolshed.g2.bx.psu.edu/repos/%s/%s' % (tool['owner'], tool['name']))
+    return tools
 
 
-def check_tutorial(tool_file, public_galaxy_servers):
-    """ Check which public Galaxy servers can run the tool in a tutorial """
-    to_conserve = []
-    to_drop = []
-    working_galaxy_servers = None
+def check_tutorial(tool_filepath, serv_tools):
+    """Check which public Galaxy servers can run the tool in a tutorial"""
+    exp_tools = extract_tool_list(tool_filepath)
     # parse the public servers
-    for index, server in public_galaxy_servers.iterrows():
-        # test if server is accessible and remove from the list if not
-        gi = test_server_connection(server['url'])
-        if gi is None:
-            to_drop.append(index)
-            continue
-        # check if the tools are accessible on the serve
-        log_file = os.path.join(log_dir, server['name'])
-        can_run = check_instance(gi, tool_file, log_file)
-        if can_run is None:
-            to_drop.append(index)
-        elif can_run:
-            to_conserve.append(index)
-    # extract the info of public server that can be used for the tutorial
-    if len(to_conserve) > 0:
-        working_galaxy_servers = public_galaxy_servers.ix[to_conserve]
-    # remove the non accessible servers
-    public_galaxy_servers = public_galaxy_servers.drop(to_drop)
-    return working_galaxy_servers, public_galaxy_servers
+    cons_servers = {}
+    for server in serv_tools:
+        # check overlap
+        res = exp_tools.issubset(serv_tools[server]['tools'])
+        if res:
+            cons_servers[server] = {'url': serv_tools[server]['url']}
+    return cons_servers
 
 
-def check_tutorials(public_galaxy_servers):
-    """ Extract a dictionary with for each topic the list tutorials to tests """
-    to_test = {}
-    os.makedirs(log_dir, exist_ok=True)
-    # remove existing log files
-    for f in os.listdir(log_dir):
-        os.remove(os.path.join(log_dir, f))
-    # parse topics
+def check_tutorials():
+    """Check all tutorials to extract the instances on which the tutorials can
+    be run and add this information to metadata/instances.yaml file"""
+    server_tools = extract_public_galaxy_servers_tools()
+    instance_annot = {}
     for topic in os.listdir("topics"):
-        to_test.setdefault(topic, [])
-        topic_path = os.path.join("topics", topic)
-        tutorials_path = os.path.join(topic_path, "tutorials")
-        # load topic metadata
-        metadata_file = os.path.join(topic_path, "metadata.yaml")
-        with open(metadata_file, 'r') as f:
-            topic_metadata = yaml.safe_load(f)
-        #print(topic_metadata)
-        # parse the tutorials of a topic
-        for tutorial in os.listdir(tutorials_path):
-            print(tutorial)
-            tutorial_path = os.path.join(tutorials_path, tutorial)
-            # pass if the tools.yaml file does not exist
-            tool_file = os.path.join(tutorial_path, 'tools.yaml')
-            if not os.path.exists(tool_file):
+        topic_dir = os.path.join("topics", topic)
+        tutos_dir = os.path.join(topic_dir, "tutorials")
+        for tutorial in os.listdir(tutos_dir):
+            # extract the tool file
+            tool_filepath = os.path.join(tutos_dir, tutorial, "tools.yaml")
+            if not os.path.exists(tool_filepath):
                 continue
-            # find the Galaxy servers with the needed tools
-            working_galaxy_servers, public_galaxy_servers = check_tutorial(tool_file, public_galaxy_servers)
-            print(working_galaxy_servers)
-            # add the information to the metadata of the tutorial
-        # write the metadata
-        with open(metadata_file, 'w') as f:
-            yaml.safe_dump(topic_metadata, f)
-        #pp.pprint(topic_metadata)
-    return to_test
+            # extract the instances on which the tutorial can be run
+            cons_servers = check_tutorial(tool_filepath, server_tools)
+            # add the conserved servers to the main dictionary if not empty
+            if not cons_servers:
+                continue
+            instance_annot.setdefault(topic, {})
+            instance_annot[topic][tutorial] = cons_servers
+    # add the conserved servers to the metadata/instances.yaml file
+    inst_file = os.path.join("metadata", "instances.yaml")
+    with open(inst_file, "w") as f:
+        yaml.dump(instance_annot, f, default_flow_style=False)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract which public Galaxy servers can run which tutorial and add the information in the metadata')
+    parser = argparse.ArgumentParser(description='Extract which public Galaxy servers can run for the tutorials and add this information to a instance file')
     args = parser.parse_args()
-
-    public_galaxy_servers = extract_public_galaxy_servers()
-    check_tutorials(public_galaxy_servers)
+    check_tutorials()
