08febefb6a0209056ab23480535c6c7f68cee126
Helena Rasche <hxr@hx42.org>
2018-08-17 16:29:09 +0200

    Thanks @shiltemann

--- _plugins/jekyll-topic-filter.rb
+++ _plugins/jekyll-topic-filter.rb
@@ -1,5 +1,10 @@
 module Jekyll
   module TopicFilter
+    def topic_count(resources)
+      # Count lines in the table except introduction slides
+      resources.select{ |a| a['type'] != 'introduction' }.length
+    end
+
     def topic_filter(pages, topic_name)
       relevant_pages = []
 
