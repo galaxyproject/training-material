0861571d9ed40dad8c3fa99a40746387b50ad631
Bérénice Batut <bebatut@gmail.com>
2018-11-19 16:46:47 +0100

    Improve the ruby plugin

--- _plugins/jekyll-notranslate.rb
+++ _plugins/jekyll-notranslate.rb
@@ -1,14 +1,8 @@
 module Jekyll
   module NoTranslate
     def no_translate(text, words)
-        for word in words do
-            word = word.downcase
-            text = text.gsub(word, "<span class=\"notranslate\">#{word}</span>")
-            word = word.capitalize
-            text = text.gsub(word, "<span class=\"notranslate\">#{word}</span>")
-            word = word.upcase
-            text = text.gsub(word, "<span class=\"notranslate\">#{word}</span>")
-        end
+        escaped_words = words.map { |word| Regexp.escape(word) }
+        text = text.gsub(/(#{escaped_words.join("|")})/i, "<span class=\"notranslate\">\\1</span>")
         text
     end
   end
