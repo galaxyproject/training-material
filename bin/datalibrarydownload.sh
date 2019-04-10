021b90988f5e457833d02a2449abce4d782d6dd1
bedro <bedro@psb.ugent.be>
2019-04-10 17:33:55 +0200

    added some comments

--- bin/datalibrarydownload.sh
+++ bin/datalibrarydownload.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-# launch the instance in deamon mode
-
 if [ "$1" = '-d' ];
 then
     echo "Starting Galaxy in the background"
+    #this is needed to use ephermeris
     startup &
     galaxy_instance="http://localhost:8080"
     galaxy-wait -g $galaxy_instance
@@ -14,6 +13,7 @@ then
     then
         echo "Downloading data from data-library_all.yaml"
         setup-data-libraries -i '/tutorials/data-library_all.yaml' -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD -v
+        #because galaxy runs in the background, it stops running after the data is downloaded. A second startupcommand is needed:
         startup
     else
         echo "No data libraries to install"
