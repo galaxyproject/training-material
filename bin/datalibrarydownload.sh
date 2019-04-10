41bc2bb474b64e3a8bc87f25759b33a3be89623b
bedro <bedro@psb.ugent.be>
2019-04-10 17:38:57 +0200

    fix startup no data libraries are avail  and -d

--- bin/datalibrarydownload.sh
+++ bin/datalibrarydownload.sh
@@ -3,20 +3,21 @@ set -e
 
 if [ "$1" = '-d' ];
 then
-    echo "Starting Galaxy in the background"
-    #this is needed to use ephermeris
-    startup &
-    galaxy_instance="http://localhost:8080"
-    galaxy-wait -g $galaxy_instance
-
     if [ "$(head -n 1 /tutorials/data-library_all.yaml)" != "{}" ];
     then
+        echo "Starting Galaxy in the background"
+        #this is needed to use ephermeris
+        startup &
+        galaxy_instance="http://localhost:8080"
+        galaxy-wait -g $galaxy_instance
         echo "Downloading data from data-library_all.yaml"
         setup-data-libraries -i '/tutorials/data-library_all.yaml' -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD -v
         #because galaxy runs in the background, it stops running after the data is downloaded. A second startupcommand is needed:
         startup
     else
         echo "No data libraries to install"
+        echo "Starting Galaxy"
+        startup
     fi
 
 else
