53d71689ee620d353bb87d042b52603b47981c76
Saskia Hiltemann <zazkia@gmail.com>
2019-07-04 13:46:30 +0200

    generate tools.yaml files from workflows before instance annotation

--- bin/workflow_to_tool_yaml.sh
+++ bin/workflow_to_tool_yaml.sh
@@ -7,15 +7,13 @@ do
     echo "Topic: $topic"
     for tutorial in $topic/tutorials/*
     do
-        echo "Tutorial: $tutorial"
+        echo " - Tutorial: $tutorial"
         if [ -d ${tutorial}/workflows ]
         then
-            for w in ${tutorial}/workflows/*.ga
-            do
-                workflow-to-tools -w $w -o ${tutorial}/tools.yaml -l $(basename ${tutorial})
-            done
+            workflow-to-tools -w ${tutorial}/workflows/*.ga -o ${tutorial}/tools.yaml -l $(basename ${tutorial})
+            echo "    ..done. Tool list in ${tutorial}/tools.yaml"
         else
-            echo "No workflows to install (no directory named workflow present)"
+            echo "    No workflows to install (no directory named workflow present)"
         fi
     done
 done
