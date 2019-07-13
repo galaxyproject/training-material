39c7eb5d614c21c314e8369ebf50fc8e6e90ab49
Saskia Hiltemann <zazkia@gmail.com>
2019-07-13 12:37:01 +0200

    move all js code to file

--- assets/js/topic.js
+++ assets/js/topic.js
@@ -1,14 +1,29 @@
+// setup tutorial list filtering
 var options = {
   valueNames: ['tutorial_title']
 };
 var tutoList = new List('tutorial_list', options)
 
 tutoList.filter(function(item) {
-if (item.values().id > 1) {
-   return true;
-} else {
+  if (item.values().id > 1) {
+     return true;
+  } else {
    return false;
-}
+  }
 }); // Only items with id > 1 are shown in list
 
-tutoList.filter(); // Remove all filters
+tutoList.filter(); // remove filters
+
+// clear search
+$('#clear_search').click(function(){
+    sb = document.getElementById('tutorial_search');
+    sb.value = '';
+    sb.dispatchEvent(new KeyboardEvent('keyup', {'key':13}));
+});
+
+// search by tutorial tag
+$('.tutorial_tag').click(function() {
+    sb = document.getElementById('tutorial_search');
+    sb.value = this.id;
+    sb.dispatchEvent(new KeyboardEvent('keyup', {'key':13}));
+});
