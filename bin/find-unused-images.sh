18653d186e88580b0275cdae2812a4dfd7e95b8e
Saskia Hiltemann <saskiahiltemann@gmail.com>
2019-09-26 15:28:48 +0200

    Rewrite to use more efficient/safe search method
    The number of false positives (those which were marked safe but should
    not have been) will increase but the false negative rate goes down (e.g.
    those doing weird document transclusion/image referencing).

--- bin/find-unused-images.sh
+++ bin/find-unused-images.sh
@@ -1,68 +1,24 @@
 #!/bin/bash
-USED=$(mktemp)
 ALL=$(mktemp)
+echo "Locating images ($ALL)"
+find topics/*/images/ -type f > $ALL
 
+overallec=0
 
-for topic in $(find topics -maxdepth 1 -mindepth 1 -type d); do
-	# get directly referenced images
-	egrep '!\[[^[]*\]\([^)]*\)' $topic/tutorials/*/slides.html $topic/tutorials/*/tutorial.md -h 2>/dev/null| \
-		grep ']([^(]*)' -o | \
-		grep -v '://' | \
-		sed 's/^](//' | \
-		grep -F .. | \
-		sed 's/ .*//g;s/)$//' | \
-		sed "s|\.\./\.\.|$topic|" | \
-		sed "s|)$||g" | \
-		xargs -n1 -I {} realpath --relative-base=$(pwd) {} \
-		>> $USED.all
+for img in $(cat "$ALL"); do
+	image_name=$(basename "$img")
+	grep -RI "$image_name" topics > /dev/null
+	ec=$?
 
-	# Linked
-	egrep '!\[[^[]*\]\([^)]*\)' $topic/tutorials/*/slides.html $topic/tutorials/*/tutorial.md -h 2>/dev/null | \
-		grep ']([^(]*)' -o | \
-		grep -v '://' | \
-		sed 's/^](//' | \
-		grep -v -F .. | \
-		grep topics | \
-		sed 's/{{\s*site.baseurl\s*}}{% link //g;s/\s*%}.*//g;s/{{site.baseurl}}\///g;s/ .*//' | \
-		sed "s|)$||g" \
-		>> $USED.all
-
-
-	# Intro slides are special
-	# get directly referenced images
-	egrep '!\[[^[]*\]\([^)]*\)' $topic/slides/*.html -h 2>/dev/null | \
-		grep ']([^(]*)' -o | \
-		grep -v '://' | \
-		sed 's/^](//' | \
-		grep -F .. | \
-		sed 's/ .*//g;s/)$//' | \
-		sed "s|\.\.|$topic|" | \
-		sed "s|)$||g" | \
-		xargs -n1 -I {} realpath --relative-base=$(pwd) {} \
-		>> $USED.all
-
-	# Linked
-	egrep '!\[[^[]*\]\([^)]*\)' $topic/slides/*.html -h 2>/dev/null | \
-		grep ']([^(]*)' -o | \
-		grep -v '://' | \
-		sed 's/^](//' | \
-		grep -v -F .. | \
-		grep topics | \
-		sed 's/{{\s*site.baseurl\s*}}{% link //g;s/\s*%}.*//g;s/{{site.baseurl}}\///g;s/ .*//' | \
-		sed "s|)$||g" \
-		>> $USED.all
+	if (( ec != 0 )); then
+		if [[ $1 == "--remove" ]]; then
+			echo "Removing $img"
+			rm "$img"
+		else
+			echo "$img appears to be unused"
+			overallec=1
+		fi
+	fi
 done
 
-find topics/*/images/ -type f > $ALL.all
-find shared/images -type f >> $ALL.all
-
-sort -u $USED.all > $USED
-sort -u $ALL.all > $ALL
-rm -f $USED.all $ALL.all
-
-#echo "Missing images"
-#diff $USED $ALL | grep '<' | sed 's/> //g'
-
-diff $USED $ALL | grep '>' | sed 's/> //g'
-
-rm -f $USED $ALL
+exit $overallec
