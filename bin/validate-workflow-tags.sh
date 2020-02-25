9f51a79d19f419b6e473367fa35343dc51d710ac
bedro <bedro@psb.ugent.be>
2020-02-25 16:54:03 +0100

    betetr linting

--- bin/validate-workflow-tags.sh
+++ bin/validate-workflow-tags.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 exit_with=0
 
+#python script to iterate over the steps in the workflow.
+function tester { 
+    python3 - <<END
+import sys 
+import json 
+
+problems = 0
+output = ["-----------------------ERROR-----------------------------"]
+with open("$1") as json_file:
+    data = json.load(json_file)
+    # Checking for 'tags' in workflow if topic is known
+    if 'tags' not in data or not data['tags'] or "$2" not in data['tags']:
+        problems += 1
+        output.append(
+            "{}. The 'tags' attribute is missing. Please add:".format(str(problems), data['name']))
+        output.append('"tags": [' + "\n\t" + '"' + "$2" + '"' + "\n]")
+
+    # Checking for 'annotation' in workflow
+    if 'annotation' not in data or not data['annotation']:
+        problems += 1
+        output.append(
+            "{}. The 'annotation' attribute is missing. Please add:".format(str(problems)))
+        output.append('"annotation": "<title of tutorial>"')
+
+    # Checking if there are tools used from the testtoolshed
+    for stepnr, step in data['steps'].items():
+        if step['tool_id'] and step['type'] == 'tool' and 'testtoolshed.g2.bx.psu.edu' in step['tool_id']:
+            problems += 1
+            output.append("{}. Step {} has a tool from the testtoolshed.".format(str(problems), str(stepnr)))
+
+    if problems:
+        output.insert(1, "Workflow '{}' has {} problem(s) because:".format(data['name'], str(problems)))
+        output.append("---------------------------------------------------------\n")
+        sys.stderr.write("\n".join(output))
+        sys.exit(False)
+    sys.exit(True)
+END
+}
+
 for topicdir in ./topics/*
 do
     topic=$(basename $topicdir)
@@ -10,13 +49,11 @@ do
         then
             for w in $tutdir/workflows/*.ga
             do
-                if [ -f $w ]
+                echo "Checking $w"
+
+                if tester $w $topic;
                 then
-                    echo "Checking $w"
-                    if planemo workflow_lint $w --topic_name $topic;
-                    then
-                        exit_with=1
-                    fi
+		            exit_with=1
                 fi
             done
         fi
