a42b13571d592a9f77177102ea1d28b523559090
bedroesb <bedro@psb.vib-ugent.be>
2020-08-07 20:53:17 +0200

    docker install scripts update

--- bin/data_libarary_download.sh
+++ bin/data_libarary_download.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# set API key and galaxy instance
+
+API_KEY=${GALAXY_DEFAULT_ADMIN_KEY:-fakekey}
+galaxy_instance="http://localhost:8080"
+
 if [ "$1" = '-d' ];
 then
     if [ "$(head -n 1 /tutorials/data-library_all.yaml)" != "{}" ];
@@ -8,10 +13,9 @@ then
         echo "Starting Galaxy in the background"
         #this is needed to use ephermeris
         startup &
-        galaxy_instance="http://localhost:8080"
         galaxy-wait -g $galaxy_instance
         echo "Downloading data from data-library_all.yaml"
-        setup-data-libraries -i '/tutorials/data-library_all.yaml' -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD -v
+        setup-data-libraries -i '/tutorials/data-library_all.yaml' -g $galaxy_instance -a $API_KEY -v
         #because galaxy runs in the background, it stops running after the data is downloaded. A second startupcommand is needed:
         startup
     else
--- bin/docker-install-tutorials.sh
+++ bin/docker-install-tutorials.sh
@@ -1,23 +1,65 @@
-# install everything for all tutorials of a topic
+#!/bin/bash
 
-set -e
-galaxy_instance="http://localhost:8080"
+# set API key
 
-# launch the instance
-echo " - Starting Galaxy.. \n"
+API_KEY=${GALAXY_DEFAULT_ADMIN_KEY:-fakekey}
 
-export GALAXY_CONFIG_TOOL_PATH=/galaxy-central/tools/
-startup_lite
+# Enable Test Tool Shed
+echo "Enable installation from the Test Tool Shed."
+export GALAXY_CONFIG_TOOL_SHEDS_CONFIG_FILE=$GALAXY_HOME/tool_sheds_conf.xml
 
-# wait until galaxy has started
-galaxy-wait -g $galaxy_instance
+. /tool_deps/_conda/etc/profile.d/conda.sh
+conda activate base
 
-# run tutorial install as user galaxy
-su - $GALAXY_USER
+if pgrep "supervisord" > /dev/null
+then
+    echo "System is up and running. Starting with the installation."
+    export PORT=80
+else
+    # start Galaxy
+    export PORT=8080
+    service postgresql start
+    install_log='galaxy_install.log'
 
-# install other tutorial materials
+    # wait for database to finish starting up
+    STATUS=$(psql 2>&1)
+    while [[ ${STATUS} =~ "starting up" ]]
+    do
+      echo "waiting for database: $STATUS"
+      STATUS=$(psql 2>&1)
+      sleep 1
+    done
+
+    echo "starting Galaxy"
+    # Unset SUDO_* vars otherwise conda run chown based on that
+    sudo -E -u galaxy -- bash -c "unset SUDO_UID; \
+        unset SUDO_GID; \
+        unset SUDO_COMMAND; \
+        unset SUDO_USER; \
+        ./run.sh -d $install_log --pidfile galaxy_install.pid --http-timeout 3000"
+
+    galaxy_install_pid=`cat galaxy_install.pid`
+    galaxy-wait -g http://localhost:$PORT -v --timeout 120
+fi
+
+# Create the admin user if not already done
+# Starting with 20.05 this user is only created at first startup of galaxy
+# We need to create it here for Galaxy Flavors = installing from Dockerfile
+if [[ ! -z $GALAXY_DEFAULT_ADMIN_USER ]]
+    then
+        (
+        cd $GALAXY_ROOT
+        . $GALAXY_VIRTUAL_ENV/bin/activate
+        echo "Creating admin user $GALAXY_DEFAULT_ADMIN_USER with key $GALAXY_DEFAULT_ADMIN_KEY and password $GALAXY_DEFAULT_ADMIN_PASSWORD if not existing"
+        python /usr/local/bin/create_galaxy_user.py --user "$GALAXY_DEFAULT_ADMIN_EMAIL" --password "$GALAXY_DEFAULT_ADMIN_PASSWORD" \
+        -c "$GALAXY_CONFIG_FILE" --username "$GALAXY_DEFAULT_ADMIN_USER" --key "$GALAXY_DEFAULT_ADMIN_KEY"
+        )
+fi
+
+# install tutorial materials
 for tutdir in $topicdir/tutorials/*
 do
+    echo "-------------------------------------------------------------"
     tut=$(basename $tutdir)
     echo "Installing tutorial: $tut"
     # install tools and workflows
@@ -31,7 +73,7 @@ do
             n=0
             until [ $n -ge 3 ]
             do
-                shed-tools install -t $tutdir/workflows/wftools.yaml -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD -v && break
+                shed-tools install -t $tutdir/workflows/wftools.yaml -a $API_KEY && break
                 n=$[$n+1]
                 sleep 5
                 echo " - Retrying shed-tools install "
@@ -39,10 +81,11 @@ do
             rm $tutdir/workflows/wftools.yaml          
         done
         echo " - Installing workflows"
-        workflow-install --publish_workflows --workflow_path $tutdir/workflows/ -g $galaxy_instance -u $GALAXY_DEFAULT_ADMIN_USER -p $GALAXY_DEFAULT_ADMIN_PASSWORD
+        workflow-install --publish_workflows --workflow_path $tutdir/workflows/ -g $galaxy_instance -a $API_KEY
     else
         echo " - No workflows to install (no directory named workflows present)"
     fi
+
 
     # install reference data? (discussion: do this at build or run time?)
     # We are using CVMFS for the moment.
@@ -73,3 +116,21 @@ done
 
 cd /tutorials/
 python /mergeyaml.py > ./data-library_all.yaml
+
+exit_code=$?
+
+if [ $exit_code != 0 ] ; then
+    if [ "$2" == "-v" ] ; then
+        echo "Installation failed, Galaxy server log:"
+        cat $install_log
+    fi
+    exit $exit_code
+fi
+
+if ! pgrep "supervisord" > /dev/null
+then
+    # stop everything
+    sudo -E -u galaxy ./run.sh --stop --pidfile galaxy_install.pid
+    rm $install_log
+    service postgresql stop
+fi
