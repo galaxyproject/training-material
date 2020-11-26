0a00763f874bb7c4b99683f9341cf096fbfd5255
Helena Rasche <hxr@hx42.org>
2020-11-26 14:36:28 +0100

    ok, even more warnings in new format

--- bin/validate-workflow.sh
+++ bin/validate-workflow.sh
@@ -2,7 +2,7 @@
 jq . < "$1" > /dev/null;
 ec=$?
 if (( ec > 0 )); then
-	echo "$1: invalid JSON"
+	echo "::error file=$1,line=0,col=0:: Invalid JSON"
 	exit 1
 fi
 
@@ -12,19 +12,19 @@ jq '.tags[]' -r < "$1" | grep -q "$topic"
 ec=$?
 
 if (( ec > 0 )); then
-	echo "$1:The tags attribute is missing. Please add: \"tags\": [\"$topic\"]"
+	echo "::warning file=$1,line=0,col=0:: The tags attribute is missing. Please add: \`\"tags\": [\"$topic\"]\`"
 	exit_with=1
 fi
 
 annotation=$(jq '.annotation' -r < "$1")
 if [[ "$annotation" == "null" ]] || [[ "$annotation" == "" ]]; then
-	echo "$1:The 'annotation' attribute is missing or incorrect. Please add: \"annotation\": \"<title of tutorial>\""
+	echo "::warning file=$1,line=0,col=0:: The 'annotation' attribute is missing or incorrect. Please add: \`\"annotation\": \"<title of tutorial>\"\`"
 	exit_with=1
 fi
 
 tts_tools=$(jq '.steps[].tool_id' -r < "$1" | grep -c testtoolshed.g2.bx.psu.edu)
 if (( tts_tools > 0 )); then
-	echo "$1:Some steps have tools from the testtoolshed. These are not permitted in GTN tutorials."
+	echo "::warning file=$1,line=0,col=0:: Some steps have tools from the testtoolshed. These are not permitted in GTN tutorials."
 	jq '.steps[].tool_id' -r < "$1" | grep -c testtoolshed.g2.bx.psu.edu
 	exit_with=1
 fi
