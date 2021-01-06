e9cf20b54b2830c98e62743501586a142577b21b
Helena Rasche <hxr@hx42.org>
2021-01-06 11:51:05 +0100

    fix build process

--- bin/ari-make.sh
+++ bin/ari-make.sh
@@ -4,10 +4,19 @@ for slides in $(find topics -name 'slides.html' | xargs ./bin/filter-has-videos)
 	dir="$(dirname "$slides")"
 	pdf="$dir/$(basename "$slides" .html).pdf"
 	mp4="$dir/$(basename "$slides" .html).mp4"
+	built_slides="_site/training-material/$slides"
 
-	# Build the slides
-	make "_site/training-material/$pdf" ACTIVATE_ENV=pwd
+	# Process the slides
+	echo $built_slides
+	cat "$built_slides" | \
+		sed "s|/training-material/|$(pwd)/_site/training-material/|g"  | \
+		sed "s|<head>|<head><base href=\"file://$(pwd)/$slides\">|" | \
+		wkhtmltopdf \
+			--enable-javascript --javascript-delay 3000 --page-width 700px --page-height 530px -B 5px -L 5px -R 5px -T 5px \
+			--user-style-sheet bin/slides-fix.css \
+			- _site/training-material/"$pdf";
 
 	# Build the slides
 	./bin/ari.sh "_site/training-material/$pdf" "$slides" "$mp4"
+	exit
 done
