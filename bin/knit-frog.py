33cc3c407df34802f6b1ee68590d092f61a2912f
Helena Rasche <hxr@hx42.org>
2021-02-16 17:17:05 +0100

    formatting, argparse to frog

--- bin/knit-frog.py
+++ bin/knit-frog.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
+import argparse
 import re
 import sys
 import knittingneedles as knit
 
+
 # read in a tutorial, and check the structure of it.
-tuto = open(sys.argv[1], 'r')
+parser = argparse.ArgumentParser(
+    description="Extract specially formatted diffs from tutorials as a series of patches to apply"
+)
+parser.add_argument("tutorial", type=argparse.FileType("r"), help="Input tutorial")
+parser.add_argument(
+    "prefix", help="Some prefix for the output commit files to keep them separate"
+)
+args = parser.parse_args()
 
 
 diffs = []
 current = None
-for line, text in enumerate(tuto.read().split('\n')):
+for line, text in enumerate(args.tutorial.read().split("\n")):
     m = re.match(knit.BOXES, text)
     if m:
-        depth = m.group(1).count('>')
+        depth = m.group(1).count(">")
     else:
         depth = 0
 
@@ -28,33 +37,28 @@ for line, text in enumerate(tuto.read().split('\n')):
     elif current is not None:
         current.append(unprefixed)
 
-    if current and len(current) > 2 and current[-2].strip() == '```' and not m2:
+    if current and len(current) > 2 and current[-2].strip() == "```" and not m2:
         current = None
 
     if m2:
         diffs.append(current)
         current = None
 
-postfix = [
-    '--',
-    '2.25.1',
-    '',
-    ''
-]
+postfix = ["--", "2.25.1", "", ""]
 
 # import sys; sys.exit()
 
 
 for idx, diff in enumerate(diffs):
     commit_msg = knit.extractCommitMsg(diff)
-    safe_commit = re.sub('[^a-z0-9-]', '-', commit_msg.lower())
+    safe_commit = re.sub("[^a-z0-9-]", "-", commit_msg.lower())
 
     prefix = [
-        'From: The Galaxy Training Network <gtn@example.org>',
-        'Date: Mon, 15 Feb 2021 14:06:56 +0100',
-        f'Subject: PATCH [{idx + 1}/{len(diffs)}]: {commit_msg}',
-        '',
-        '',
+        "From: The Galaxy Training Network <gtn@example.org>",
+        "Date: Mon, 15 Feb 2021 14:06:56 +0100",
+        f"Subject: PATCH [{idx + 1}/{len(diffs)}]: {commit_msg}",
+        "",
+        "",
     ]
 
     (_, diff) = knit.removeWhitespacePrefix(diff)
@@ -62,10 +66,10 @@ for idx, diff in enumerate(diffs):
     patch_id = diff[-1]
     # Remove patch id, ```
     diff = diff[0:-2]
-    if diff[-1] == '{% endraw %}':
+    if diff[-1] == "{% endraw %}":
         diff = diff[0:-1]
 
-    fn = f'commit-{idx:04d}-{safe_commit}.patch'
-    with open(fn, 'w') as handle:
+    fn = f"{args.prefix}-commit-{idx:04d}-{safe_commit}.patch"
+    with open(fn, "w") as handle:
         print(fn)
-        handle.write('\n'.join(prefix + diff + postfix))
+        handle.write("\n".join(prefix + diff + postfix))
--- bin/knit.py
+++ bin/knit.py
@@ -4,18 +4,18 @@ import sys
 import knittingneedles as knit
 
 # read in a tutorial, and check the structure of it.
-tuto = open(sys.argv[1], 'r+')
-tutorial_contents = tuto.read().split('\n')
+tuto = open(sys.argv[1], "r+")
+tutorial_contents = tuto.read().split("\n")
 chunks = []
 patches = sys.argv[2:]
 
 # load patches
 diffs = []
 for patch in patches:
-    with open(patch, 'r') as handle:
+    with open(patch, "r") as handle:
         data = handle.readlines()
-        commit = data[2].split(': ')[2]
-        data = [x.rstrip('\n') for x in data[5:-3]]
+        commit = data[2].split(": ")[2]
+        data = [x.rstrip("\n") for x in data[5:-3]]
         diffs.append((commit, data))
 
 current = None
@@ -24,7 +24,7 @@ prev_line = None
 for line, text in enumerate(tutorial_contents):
     m = re.match(knit.BOXES, text)
     if m:
-        depth = m.group(1).count('>')
+        depth = m.group(1).count(">")
     else:
         depth = 0
     (unprefixed, prefix) = knit.stripN(text, depth)
@@ -39,7 +39,7 @@ for line, text in enumerate(tutorial_contents):
         current = []
         # print('hi', m0, m1, prev_line)
         # if '{% raw %}' in prev_line:
-            # current.append(prev_line)
+        # current.append(prev_line)
 
     if current is not None:
         current.append(unprefixed)
@@ -47,9 +47,9 @@ for line, text in enumerate(tutorial_contents):
         chunks.append(text)
 
     # if current:
-        # print(current, len(current), not m2)
+    # print(current, len(current), not m2)
 
-    if current and len(current) > 2 and (current[-2].strip() == '```') and not m2:
+    if current and len(current) > 2 and (current[-2].strip() == "```") and not m2:
         chunks.extend([prefix + x for x in current])
         # print(current)
         # chunks.append('REJECT)')
@@ -68,17 +68,13 @@ for line, text in enumerate(tutorial_contents):
         if obs_msg != exp_msg:
             print("WARNING: Diff messages do NOT line up: {obs_msg} != {exp_msg}")
 
-
         # Knit it anyway
         new_diff = from_patch[1]
-        chunks.append(prefix_text + '{% raw %}')
-        chunks.append(prefix_text + '```diff')
-        chunks.extend([
-            f'{prefix}{amount * " "}{line}'
-            for line in new_diff
-        ])
-        chunks.append(prefix_text + '{% endraw %}')
-        chunks.append(prefix_text + '```')
+        chunks.append(prefix_text + "{% raw %}")
+        chunks.append(prefix_text + "```diff")
+        chunks.extend([f'{prefix}{amount * " "}{line}' for line in new_diff])
+        chunks.append(prefix_text + "{% endraw %}")
+        chunks.append(prefix_text + "```")
         chunks.append(prefix_text + '{: data-commit="%s"}' % exp_msg)
         diff_idx += 1
         current = None
@@ -90,5 +86,5 @@ tuto.seek(0)
 tuto.truncate(0)
 # The last chunk is a newline for some reason.
 for c in chunks[0:-1]:
-    tuto.write(c + '\n')
+    tuto.write(c + "\n")
 tuto.close()
