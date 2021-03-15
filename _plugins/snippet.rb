f7be44699c2f7a2ac0bf92d5660736af0c2c7ee1
Saskia Hiltemann <zazkia@gmail.com>
2021-03-15 17:09:57 +0100

    don't add tip boxes to toc

--- _plugins/snippet.rb
+++ _plugins/snippet.rb
@@ -68,7 +68,7 @@ module Jekyll
              #puts box_start+y+box_end
           end
 
-          markdownify(box_start+y+box_end).gsub(/\R+/, '')
+          markdownify(box_start+y+box_end).gsub(/\R+/, '').gsub('<h3','<h3 data-toc-skip')
         end
       end
 
