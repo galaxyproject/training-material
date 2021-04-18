5ec23cc2c33a76da1fa0dae2b8724266cbcc729d
fubar2 <ross.lazarus@gmail.com>
2021-04-18 20:59:50 +1000

    hickory dickory dockery

--- topics/dev/tutorials/tool-generators/docker/start.sh
+++ topics/dev/tutorials/tool-generators/docker/start.sh
@@ -1,7 +1,8 @@
 # please adjust as needed and save as start.sh
 # the volume can be populated with newly generated tools from unpacked toolshed archives and remains persistent
 # history WILL NOT persist so save it if you want to import it in the future to save typing all your work again.
-docker run -d -p 8080:80 -p 9090:9090  \
-    -v /home/ross/rossgit/training-material/mytools:/planemo/mytools \
-    tf-tutorial
+docker run -d -p 8080:8080 -p 9090:9090  \
+    -v /home/ross/rossgit/planemo/mytools:/planemo/mytools \
+    quay.io/fubar2/toolfactory_tutorial:latest
+### -v /var/run/docker.sock:/var/run/docker.sock \
 
