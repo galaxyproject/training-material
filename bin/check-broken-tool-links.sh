814a3e33177a1ae458460bc10d58289d32f7003e
Bérénice Batut <berenice.batut@gmail.com>
2021-05-27 10:29:07 +0200

    Add ChangeCaseCount1 to the whitelist of tool ids

--- bin/check-broken-tool-links.sh
+++ bin/check-broken-tool-links.sh
@@ -4,7 +4,7 @@ output=$(mktemp)
 # Check the toolshed-y ones.
 grep -n '\{% tool \[' -R topics | sed 's/:\([0-9]\+\):.*{% tool/\t\1/g;s/)\s*%}.*//g;s/ \[/\t/g;s/](/\t/' | \
 	awk -F'\t' '(substr($4,1,18) != "toolshed.g2.bx.psu"){ print $0 }' | \
-	egrep -i -v '(intermine|Count1|Cut1|Filter1|Grep1|Grouping1|Paste1|Remove\+beginning1|Show\+beginning1|Summary_Statistics1|addValue|cat1|comp1|gene2exon1|join1|random_lines1|ucsc_table_direct1|upload1|sort1|wig_to_bigWig)' | \
+	egrep -i -v '(intermine|Cut1|Filter1|Grep1|Grouping1|Paste1|Remove\+beginning1|Show\+beginning1|Summary_Statistics1|addValue|cat1|comp1|gene2exon1|join1|random_lines1|ucsc_table_direct1|upload1|sort1|wig_to_bigWig|ChangeCaseCount1)' | \
     egrep -v '__[A-Z_]+__' | \
 	egrep -v 'interactive_tool_[a-z]+' | \
 	awk -F'\t' '{print "::warning file="$1",line="$2",col=0::Broken tool link, please double check."}' | tee $output
