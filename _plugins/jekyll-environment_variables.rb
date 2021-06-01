9bb09e3e477eeda4e9b750d771a314c8b63e8831
Helena Rasche <hxr@hx42.org>
2021-06-01 14:46:38 +0200

    add support for archive menu

--- _plugins/jekyll-environment_variables.rb
+++ _plugins/jekyll-environment_variables.rb
@@ -14,6 +14,7 @@ module Jekyll
       end
       site.config['git_revision'] = git_head_ref
       site.config['gtn_fork'] = ENV['GTN_FORK']
+      site.config['git_tags'] = `git tag -l`.strip.split
 
       # Add other environment variables to `site.config` here...
     end
