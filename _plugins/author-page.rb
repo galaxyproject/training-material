865166b25ba755f5d3ea91548bf1aca21a2463ce
Helena Rasche <hxr@hx42.org>
2021-10-04 14:44:26 +0200

    Replace method for generating HoF pages
    This should be a performance improvement but it is hard to see initially
    because previously they were registered as separate 'pages' and now it's
    collected as a single page. Which does help identify potential
    performance blockers though. And now we can e.g. centralise all of the
    'find all tutorials for this author' step which was probably quite
    slow.'
    
    This also removes the need for the 'annoate-page' filter which was not
    very useful in the first place'

--- _plugins/author-page.rb
+++ _plugins/author-page.rb
@@ -5,34 +5,42 @@ module Jekyll
     def generate(site)
       if site.layouts.key? 'contributor_index'
         dir = 'hall-of-fame'
+
+        # pre-calculating this hash saves about 2 seconds off the previous
+        # build time of 5 seconds.
+        tutorials_by_author = Hash.new { |hash, key| hash[key] = [] }
+        slides_by_author = Hash.new { |hash, key| hash[key] = [] }
+
+        site.pages.select{|t| t['layout'] == 'tutorial_hands_on'}.each{|t|
+          t['contributors'].each{|c|
+            tutorials_by_author[c].push(t)
+          }
+        }
+
+        site.pages.select{|t| ! ['base_slides', 'introduction_slides', 'tutorial_slides'].index(t['layout']).nil?}.each{|t|
+          t['contributors'].each{|c|
+            slides_by_author[c].push(t)
+          }
+        }
+
         site.data['contributors'].each_key do |contributor|
-          site.pages << AuthorPage.new(site, site.source, File.join(dir, contributor), contributor)
+          # Using PageWithoutAFile instead of a custom class which reads files
+          # from disk each time, saves some time, but it is unclear how much
+          # due to how the previous was accounted. But assuming 0.040s per page * 193 should be about 8 seconds.
+          page2 = PageWithoutAFile.new(site, "", File.join(dir, contributor), "index2.html")
+          page2.content = nil
+          name = site.data['contributors'][contributor].fetch('name', contributor)
+
+          # Their tutorials
+          page2.data["contributor"] = contributor
+          page2.data['personname'] = name
+          page2.data['title'] = "GTN Contributor: #{name}"
+          page2.data["layout"] = "contributor_index"
+          page2.data["tutorials"] = tutorials_by_author[contributor]
+          page2.data["slides"] = slides_by_author[contributor]
+          site.pages << page2
         end
       end
-    end
-  end
-
-  # A Page subclass used in the `AuthorPageGenerator`
-  class AuthorPage < Page
-    def initialize(site, base, dir, contributor)
-      @site = site
-      @base = base
-      @dir  = dir
-      @name = 'index.html'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'contributor_index.html')
-      self.data['contributor'] = contributor
-
-      if @site.data['contributors'][contributor] and @site.data['contributors'][contributor].has_key?('name') then
-        name = @site.data['contributors'][contributor]['name']
-      else
-        name = contributor
-      end
-
-      self.data['personname'] = name
-
-      self.data['title'] = "GTN Contributor: #{name}"
     end
   end
 end
