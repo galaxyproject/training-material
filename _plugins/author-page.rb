e65c0b4f1b66183428260f28fd4e3f770e006f5f
Helena Rasche <hxr@hx42.org>
2021-10-11 14:05:59 +0200

    Improve the generation of author pages

--- _plugins/author-page.rb
+++ _plugins/author-page.rb
@@ -13,19 +13,20 @@ module Jekyll
         has_philosophy = Hash.new { false }
 
         site.pages.each {|t|
-          # TUtorials
-          if t['layout'] == 'tutorial_hands_on'
-            t['contributors'].each{|c| tutorials_by_author[c].push(t) }
+          # Tutorials
+          if t['layout'] == 'tutorial_hands_on' && ! t.data['contributors'].nil?
+            puts "#{t.path} #{t.data}"
+            t.data['contributors'].each{|c| tutorials_by_author[c].push(t) }
           end
 
           # Slides
-          if ! ['base_slides', 'introduction_slides', 'tutorial_slides'].index(t['layout']).nil?
-            t['contributors'].each{|c| slides_by_author[c].push(t) }
+          if ! ['base_slides', 'introduction_slides', 'tutorial_slides'].index(t['layout']).nil? && ! t.data['contributors'].nil?
+            t.data['contributors'].each{|c| slides_by_author[c].push(t) }
           end
 
           # Philosophies
-          if t['layout'] == 'training_philosophy'
-            has_philosophy[t['username']] = true
+          if t['layout'] == 'training_philosophy' && ! t.data['username'].nil?
+            has_philosophy[t.data['username']] = true
           end
         }
 
@@ -33,7 +34,7 @@ module Jekyll
           # Using PageWithoutAFile instead of a custom class which reads files
           # from disk each time, saves some time, but it is unclear how much
           # due to how the previous was accounted. But assuming 0.040s per page * 193 should be about 8 seconds.
-          page2 = PageWithoutAFile.new(site, "", File.join(dir, contributor), "index2.html")
+          page2 = PageWithoutAFile.new(site, "", File.join(dir, contributor), "index.html")
           page2.content = nil
           name = site.data['contributors'][contributor].fetch('name', contributor)
 
