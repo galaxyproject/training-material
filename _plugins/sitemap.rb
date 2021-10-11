7f80b3aef7a9caebc476751da5d0717c990efad0
Helena Rasche <hxr@hx42.org>
2021-10-11 16:26:44 +0200

    rescue last modified times

--- _plugins/sitemap.rb
+++ _plugins/sitemap.rb
@@ -9,10 +9,17 @@ module Jekyll
       result = '<?xml version="1.0" encoding="UTF-8"?>'
       result += '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
-      site.pages.each {|t|
-        d = t.data['last_modified_at']
-        d.format = '%FT%T%:z'
-        result += "<url><loc>#{site.config['url'] + site.config['baseurl'] + t.url}</loc><lastmod>#{d}</lastmod></url>"
+      site.pages.select{|t| t.path !~ /ipynb$/ }.each {|t|
+        begin
+          d = t.data['last_modified_at']
+          d.format = '%FT%T%:z'
+          formatted_date = d.to_s
+        rescue
+          d = Time.new
+          formatted_date = d.strftime('%FT%T%:z')
+        end
+
+        result += "<url><loc>#{site.config['url'] + site.config['baseurl'] + t.url}</loc><lastmod>#{formatted_date}</lastmod></url>"
       }
       result += "</urlset>"
 
