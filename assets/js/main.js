762e7ce48d2896fa7029c7733f36f753f9dd776a
shiltemann <shiltemann@users.noreply.github.com>
2022-10-18 09:57:25 +0000

    deploy: 7b1e1c0e2204ea5fb6194147fe90a432dd2c7961

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,50 @@
 // make boxes collapsible
+//LEGACY
 $(".solution>h3,.details>h3,.tip>h3,.question>h3,.hands_on>h3,.comment>h3").click(function(event) {
     $(">*:not(h3)", $(this).parent()).toggle(400);
     $(">span.fold-unfold", this).toggleClass("fa-plus-square fa-minus-square");
 });
 
+//NEW
+$("blockquote.solution>.box-title>button,blockquote.details>.box-title>button,blockquote.tip>.box-title>button,blockquote.question>.box-title>button,blockquote.hands_on>.box-title>button,blockquote.comment>.box-title>button").click(function(event) {
+	// If they click the icons, we need to make sure we have a reference to the button properly
+	var button;
+	if(event.target.nodeName === "BUTTON"){
+		button = $(event.target)
+	} else {
+		button = $(event.target).parents("button");
+	}
+	// Then we can collapse specifically things based on the button's ancestry
+	var parentBlockquote = button.parents("blockquote")[0];
+
+	// Collapse every child of the blockquote, that is NOT a box-title
+    $(">*:not(.box-title)", parentBlockquote).toggle();
+	// And toggle our icon
+    $(">span.fold-unfold", button).toggleClass("fa-plus-square fa-minus-square");
+
+	// Naturally we also need to toggle the aria-expanded attribute to make sure we're accessible
+    $(this).attr("aria-expanded",
+		// if it's collapsed
+		$(">span.fold-unfold", this).hasClass("fa-plus-square") ? 
+		// mark as collapsed
+		"true" : "false"
+	);
+});
+
 // collapse some box types by default
-$(".solution,.details,.tip").each(function() {
-    $(">*:not(h3)", this).toggle();
-    var h3 = $("h3:first", this);
-    h3.append("<span role='button' class='fold-unfold fa fa-plus-square'></span>");
+// LEGACY
+ $(".solution>h3,.details>h3,.tip>h3").each(function() {
+    $(">*:not(h3)", $(this.parent)).toggle();
+    $(this).append("<span role='button' class='fold-unfold fa fa-plus-square'></span>");
 });
 
-// don't collapse the others by default
-$(".question,.hands_on,.comment").each(function() {
-    var h3 = $("h3:first", this);
-    h3.append("<span role='button' class='fold-unfold fa fa-minus-square'></span>");
+
+// NEW
+$("blockquote.solution,blockquote.details,blockquote.tip").each(function() {
+    $(">.box-title>button", this).click();
 });
 
-$("section.tutorial .hands_on").append('<p class="text-muted" style="text-align:right;font-size:0.9rem;"><i class="far fa-question-circle" aria-hidden="true"></i> <a href="./faqs/">FAQs</a> | <a href="https://gitter.im/Galaxy-Training-Network/Lobby">Gitter Chat</a> | <a href="https://help.galaxyproject.org">Help Forum</a></p>');
+$("section.tutorial .hands_on,section.tutorial .hands-on").append('<p class="text-muted" style="text-align:right;font-size:0.9rem;"><i class="far fa-question-circle" aria-hidden="true"></i> <a href="./faqs/">FAQs</a> | <a href="https://gitter.im/Galaxy-Training-Network/Lobby">Gitter Chat</a> | <a href="https://help.galaxyproject.org">Help Forum</a></p>');
 
 // CYOA Support
 function cyoaChoice(text){
@@ -85,12 +112,12 @@ function cyoaDefault(defaultOption){
 
         var expandAll = (new URL(document.location)).searchParams.get("expand-all");
         if (expandAll !== null) {
-            $(".solution>h3,.details>h3,.tip>h3").click();
-        }
+            $(".solution>.box-title,.details>.box-title,.tip>.box-title").click();
 
+        }
         // collapse all boxes on the faq overview pages
         if (window.location.href.indexOf("faqs") > -1) {
-            $(".hands_on>h3,.question>h3,.comment>h3").click();
+            $(".hands_on>.box-title,.question>.box-title,.comment>.box-title").click();
         }
     });
 
