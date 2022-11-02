16a41f48a00d5e81e63c6aa39f3f5ca7f00cf491
Helena Rasche <hxr@hx42.org>
2022-11-02 11:01:39 +0100

    More unnecessary stuff

--- _plugins/gtn/boxify.rb
+++ _plugins/gtn/boxify.rb
@@ -123,12 +123,14 @@ module Gtn
       "#{@@BOX_TITLES[lang][box_type]}#{title_fmted}"
     end
 
-    def self.generate_collapsible_title(box_type, title, lang="en", key)
+    def self.generate_collapsible_title(box_type, title, lang="en", key, contents: false)
       box_id = self.get_id(box_type, title, key)
       box_title = self.format_box_title(title, box_type, lang=lang)
+      refers_to_contents = contents ? "-contents": ""
+      # These are all collapsed by default, details, tip, and solution.
       return [box_id, %Q(
         <div class="box-title #{box_type}-title" id="#{box_id}">
-        <button class="gtn-boxify-button #{box_type}" type="button" aria-controls="#{box_id}-contents" aria-expanded="true" aria-label="Toggle #{box_type} box: #{title}">
+        <button class="gtn-boxify-button #{box_type}" type="button" aria-controls="#{box_id}#{refers_to_contents}" aria-expanded="true">
           #{self.get_icon(box_type)} #{box_title}
           <span role="button" class="fold-unfold fa fa-minus-square"></span>
         </button>
@@ -145,7 +147,7 @@ module Gtn
       end
 
       return [box_id, %Q(
-        <div class="box-title #{box_type}-title" id="#{box_id}" aria-label="#{box_type} box: #{title}">
+        <div class="box-title #{box_type}-title" id="#{box_id}">
           #{self.get_icon(box_type)} #{box_title}
         </div>
       ).split(/\n/).map{|x| x.lstrip.rstrip}.join("").lstrip.rstrip]
@@ -160,10 +162,10 @@ module Gtn
       title
     end
 
-    def self.generate_title(box_type, title, lang="en", key)
+    def self.generate_title(box_type, title, lang="en", key, contents: false)
       title = self.safe_title(title)
       if @@COLLAPSIBLE_BOXES.include?(box_type)
-        self.generate_collapsible_title(box_type, title, lang, key)
+        self.generate_collapsible_title(box_type, title, lang, key, contents: contents)
       else
         self.generate_static_title(box_type, title, lang, key)
       end
@@ -171,7 +173,7 @@ module Gtn
 
     def self.generate_box(box_type, title, lang="en", key)
       title = self.safe_title(title)
-      box_id, box_title = generate_title(box_type, title, lang, key)
+      box_id, box_title = generate_title(box_type, title, lang, key, contents: true)
       return %Q(
         <div class="box #{box_type}" markdown=0>
         #{box_title}
