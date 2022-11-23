26435ade21e5ada0a72271cb9d8856a58917959b
fubar2 <ross.lazarus@gmail.com>
2022-11-23 18:30:41 +1100

    update update.sh to fix both files for video link

--- update.sh
+++ update.sh
@@ -2,4 +2,8 @@
 sudo cp -r /home/ross/rossgit/training-material/_site/* /var/www/html/
 sudo sed -i 's+href="/training-material/videos/watch.html?v=galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
  /var/www/html/home/ross/rossgit/training-material/_site/training-material/topics/galaxy-project/index.html
+ 
+ sudo sed -i 's+href="/training-material/videos/watch.html?v=galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
+ /var/www/html/home/ross/rossgit/training-material/_site/training-material/topics/galaxy-project/slides/introduction.html
+ 
 sudo chown -R www-data /var/www/html/training-material
