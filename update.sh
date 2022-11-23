c74c1e1a4306e670471d1d24d8daff65a8161625
fubar2 <ross.lazarus@gmail.com>
2022-11-23 18:37:47 +1100

    and agen

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #update the copy served on ross's home server 
 sudo cp -r /home/ross/rossgit/training-material/_site/* /var/www/html/
 sudo sed -i 's+href="/training-material/videos/watch.html?v=galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
- /var/www/html/training-material/_site/training-material/topics/galaxy-project/index.html
+ /var/www/html/training-material/topics/galaxy-project/index.html
  
  sudo sed -i 's+href="/training-material/videos/watch.html?v=galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
  /var/www/html/training-material/topics/galaxy-project/slides/introduction.html
