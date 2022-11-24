fd95403aa77c606b16250fc1a820f23fda80e678
fubar2 <ross.lazarus@gmail.com>
2022-11-24 15:29:25 +1100

    remove bogus v parameter from sed call

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #update the copy served on ross's home server 
 sudo cp -r /home/ross/rossgit/training-material/_site/* /var/www/html/
-sudo sed -iv 's+href="/training-material/videos/watch.html?v=/galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
+sudo sed -i 's+href="/training-material/videos/watch.html?v=/galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
  /var/www/html/training-material/topics/galaxy-project/index.html
  
-sudo sed -iv 's+href="/training-material/videos/watch.html?v=/galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
+sudo sed -i 's+href="/training-material/videos/watch.html?v=/galaxy-project/slides/introduction"+href="/training-material/videos/introduction.mp4"+g' \
  /var/www/html/training-material/topics/galaxy-project/slides/introduction.html
  
 sudo chown -R www-data /var/www/html/training-material
