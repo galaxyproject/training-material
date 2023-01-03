cb49228f0403409c9083b93819635753e120cc5c
Helena Rasche <hexylena@galaxians.org>
2023-01-03 12:54:20 +0100

    start moving functions into gtn module for convenience

--- _plugins/gtn.rb
+++ _plugins/gtn.rb
@@ -1,2 +1,13 @@
 require './_plugins/gtn/boxify'
 require './_plugins/gtn/mod'
+
+
+module Jekyll
+  module GtnFunctions
+    def gtn_mod_date(path)
+      Gtn::ModificationTimes.obtain_time(path)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::GtnFunctions)
