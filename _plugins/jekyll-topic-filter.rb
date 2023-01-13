36c42c52fbf87063c78d47772785a962e6460c88
Helena Rasche <hexylena@galaxians.org>
2023-01-13 14:25:30 +0100

    Reorganise and simplify the topic filter

--- _plugins/jekyll-topic-filter.rb
+++ _plugins/jekyll-topic-filter.rb
@@ -8,29 +8,26 @@ module TopicFilter
     site.data.select{|k, v| v.is_a?(Hash) && v.has_key?('maintainers')}.map{|k, v| k}
   end
 
-  def self.topic_filter(site, topic_name)
+  def self.fill_cache(site)
     if not site.data.has_key?('cache_topic_filter')
+      puts "[GTN/TopicFilter] Begin Cache"
       site.data['cache_topic_filter'] = Hash.new
 
       # For each topic
       self.list_topics(site).each{|topic|
-        site.data['cache_topic_filter'][topic] = self.run_topic_filter(site.pages, topic)
+        site.data['cache_topic_filter'][topic] = self.run_topic_filter(site, site.pages, topic)
       }
+      puts "[GTN/TopicFilter] End Cache"
     end
+  end
 
+  def self.topic_filter(site, topic_name)
+    self.fill_cache(site)
     site.data['cache_topic_filter'][topic_name]
   end
 
   def self.fetch_tutorial_material(site, topic_name, tutorial_name)
-    if not site.data.has_key?('cache_topic_filter')
-      site.data['cache_topic_filter'] = Hash.new
-
-      # For each topic
-      self.list_topics(site).each{|topic|
-        site.data['cache_topic_filter'][topic] = self.run_topic_filter(site.pages, topic)
-      }
-    end
-
+    self.fill_cache(site)
     site.data['cache_topic_filter'][topic_name].select{|p| p['tutorial_name'] == tutorial_name}[0]
   end
 
@@ -40,7 +37,68 @@ module TopicFilter
     out
   end
 
-  def self.discover_files(pages)
+  def self.annotate_path(path)
+    parts = path.split('/')
+    if parts[0] == '.'
+      parts.shift
+    end
+
+    if parts[0] != 'topics'
+      return nil
+    end
+
+    if parts[2] != 'tutorials'
+      return nil
+    end
+
+    if parts.length < 4
+      return nil
+    end
+
+    material = {
+      "topic" => parts[1], # Duplicate
+      "topic_name" => parts[1],
+      "material" => parts[1] + '/' + parts[3],
+      "tutorial_name" => parts[3],
+      "dir" => parts[0..3].join('/'),
+    }
+
+    if path =~ /\/faqs\//
+      return nil
+    end
+
+    if parts[-1] =~ /data[_-]library.yaml/ || parts[-1] =~ /data[_-]manager.yaml/
+      return nil
+    end
+
+    if parts[-1] == 'tools.yaml'
+      return nil
+    end
+
+    if parts[4] =~ /tutorial.*\.md/
+      material['type'] = 'tutorial'
+    elsif parts[4] =~ /slides.*\.html/
+      material['type'] = 'slides'
+    elsif parts[4] =~ /ipynb$/
+      material['type'] = 'ipynb'
+    elsif parts[4] =~ /Rmd$/
+      material['type'] = 'rmd'
+    elsif parts[4] == 'workflows'
+      material['type'] = 'workflow'
+    elsif parts[4] == 'tours'
+      material['type'] = 'tour'
+    elsif parts[-1] == 'index.md'
+      return nil
+    else
+      return nil
+      # material['type'] = 'unknown'
+    end
+
+    return material
+
+  end
+
+  def self.collate_materials(pages)
     # In order to speed up queries later, we'll store a set of "interesting"
     # pages (i.e. things that are under `topic_name`)
     interesting = {}
@@ -48,220 +106,187 @@ module TopicFilter
       page_parts = page.url.split('/')
       # Skip anything outside of topics.
       if not page.url.include?('/topics/') then next end
+
+      # Extract the material metadata based on the path
       page.data['url'] = page.url
-      # If the path is long enough and it is under the topic name
-      if page_parts.length > 3
-        # Automate the tutorial-name thing. This writes back to the shared
-        # data structure.
-        page.data['topic_name'] = page_parts[2]
+      material_meta = self.annotate_path(page.path)
 
-        # Slides are one directory level shorter and re-use name for their identity.
-        if page_parts[3] == 'slides' then
-          page.data['tutorial_name'] = page_parts[4].sub(/\.html$/, '')
-        else
-          page.data['tutorial_name'] = page_parts[4]
-        end
+      # If unannotated then we want to skip this material.
+      if material_meta.nil? then next end
 
-        # And then store in our interesting stuff
-        key = page.url.sub(/^\//, '') # strip leading slash since later queries don't have it.
-        interesting[key] = {
-          "page" => page,
-          "topic" => page_parts[2],
-          "tutorial_name" => page.data['tutorial_name'],
-          "tags" => page.data.fetch('tags', []),
-        }
-        interesting[key] = page
+      mk = material_meta['material']
+
+      if not interesting.has_key? mk
+        interesting[mk] = material_meta.dup
+        interesting[mk].delete("type") # Remove the type since it's specific, not generic
+        interesting[mk]["resources"] = []
       end
+
+      page.data['topic_name'] = material_meta['topic_name']
+      page.data['tutorial_name'] = material_meta['tutorial_name']
+
+      interesting[mk]["resources"].push([material_meta["type"], page])
     end
 
     interesting
   end
 
-  def self.run_topic_filter(pages, topic_name)
-    # Arrays that will store all introduction slides and tutorials we discover.
-    resource_intro = []
-    resource_pages = []
-
-    interesting = self.discover_files(pages)
-    puts "[GTN/TopicFilter] #{topic_name}"
-    #require 'pp'
-    #pp interesting
-
-    #1/0
-
-    # Theory is as follows:
-    #
-    # for each folder we need to extract
-    #   if any slides/workflows/tutorials/etc
-    #
-    # and we need to update this object with the tutorial's title in order to
-    # make the object complete. It is called a page_obj or a resource within
-    # the context of the script but basically it's the same as the old
-    # entries from the metadata.yaml file. A thing that has slides:yes/no,
-    # hands_on:yes/no, and all of the other associated data so we can
-    # construct the single line in the topic page with the tutorial's
-    # resources.
-
-    # All of the files in the intro slides and tutorials directories
-    intro_slides = Dir.glob("topics/#{topic_name}/slides/*").sort
-    tutorial_folders = Dir.glob("topics/#{topic_name}/tutorials/*").sort
-
-    # Turn these into an object. Override the slides to true (since they're
-    # slides) and the type to introduction, to prevent people from having to
-    # set those variables. All other variables will be copied directly with
-    # the `page.data.dup` so people can use external hands ons and similar.
-    for intro_slide in intro_slides do
-      page = interesting[intro_slide]
-      page_obj = page.data.dup
-      page_obj['slides'] = true
-      page_obj['type'] = 'introduction'
-      resource_intro.push(page_obj)
-    end
-
-    # Look in every /topic/*/tutorials/* folder, and turn these disparate
+  def self.resolve_material(material)
+    # We've already
+    # looked in every /topic/*/tutorials/* folder, and turn these disparate
     # resources into a page_obj as well. Most variables are copied directly,
     # either from a tutorial, or a slides (if no tutorial is available.) This
     # means we do not (cannot) support external_slides AND external_handson.
     # This is probably a sub-optimal situation we'll end up fixing someday.
-    for folder in tutorial_folders do
-      # Discover resources, defined as any file in a given tutorial folder
-      resources = Dir.glob("#{folder}/*").map{ |a| a.split('/')[-1] }
+    #
+    tutorials = material["resources"].select{|a| a[0] == 'tutorial'}
+    slides    = material["resources"].select{|a| a[0] == 'slides'}
+    tours     = material["resources"].select{|a| a[0] == 'tours'}
 
-      # We will compare this against the 'interesting' hash we built earlier,
-      # let's find everything from interesting that is in this specific topic
-      # AND tutorial (interesting only contained things in this topic).
-      # We add the trailing slash because tutorial directories sharing a
-      # common prefix would cause issues.
-      known_pages = interesting.select{|a| a.include?("#{folder}/")}
+    # Our final "page" object (a "material")
+    page = nil
 
-      # We need to extract metadata from some of the pages to determine the
-      # proper 'title' attribute for a tutorial. We can only pull this if we
-      # have the page object in jekyll to work with. So we'll look for the
-      # specific keys in `known_pages` (keyed on filesystem paths) and select
-      # for the things we're interested in.
-      tutorial_page_keys = known_pages.keys.select{|a| a.include?('tutorial.html')}
-      slides_page_keys   = known_pages.keys.select{|a| a.include?('slides.html')}
-
-      # We'll handle slides first and have hands-on override.
-      page = false
-      slide_has_video = false
-      slide_translations = []
-      if slides_page_keys.length == 1 then
-        page = interesting[slides_page_keys[0]]
-        slide_has_video = page.data.fetch('video', false)
-        slide_translations = page.data.fetch('translations', [])
-      end
-
-      tutorial_translations = []
-      if tutorial_page_keys.length == 1 then
-        page = interesting[tutorial_page_keys[0]]
-        tutorial_translations = page.data.fetch('translations', [])
-      end
-
-      # If no tutorial OR slides are found, then we have an issue
-      if page == false then
-        puts "Error? No tutorial OR slides found in #{folder}. We saw #{known_pages.keys}"
-        next
-      end
-
-      # Otherwise clone the metadata from it which works well enough.
-      page_obj = page.data.dup
-
-      # Sometimes `hands_on` is set to something like `external`, in which
-      # case it is important to not override it. So we only do that if the
-      # key isn't already set. Then we choose to set it to a test for the
-      # tutorial being present. We probably don't need to test both, but it
-      # is hard to follow which keys are which and safer to test for both in
-      # case someone edits the code later. If either of these exist, we can
-      # automatically set `hands_on: true`
-      if not page_obj.has_key?("hands_on") then
-        page_obj['hands_on'] = resources.include?('tutorial.md') or resources.include?('tutorial.html')
-      end
-
-      # Same for slides, if there's a resource by that name, we can
-      # automatically set `slides: true`
-      if not page_obj.has_key?("slides") then
-        page_obj['slides'] = resources.include?('slides.html')
-      end
-
-      if resources.include?("quiz") then
-        ymls = Dir.glob("#{folder}/quiz/*.yml") + Dir.glob("#{folder}/quiz/*.yaml")
-        quizzes = ymls.map{ |a| a.split('/')[-1] }
-        page_obj['quiz'] = quizzes.map{|q|
-          quiz_data = YAML.load_file("#{folder}/quiz/#{q}")
-          {
-            "id" => q,
-            "path" => "#{folder}/quiz/#{q}",
-            "title" => quiz_data['title'],
-            "contributors" => quiz_data['contributors'],
-          }
-        }
-      end
-
-      # Similar as above.
-      if resources.include?('workflows')
-        workflow_files = Dir.glob("#{folder}/workflows/*.ga").map{ |a| a.split('/')[-1] }
-        page_obj['workflows'] = workflow_files.map{|wf|
-          x = {
-            "workflow" => wf,
-            "tests" => Dir.glob("#{folder}/workflows/" + wf.gsub(/.ga/, '-test*')).length > 0,
-          }
-          x
-        }
-      end
-
-      # Tool List
-      #
-      # This is exposed in the GTN API to help admins/devs easily get the tool
-      # list for installation.
-      page_obj['tools'] = []
-      if page_obj['hands_on']
-        page_obj['tools'] += page.content.scan(/{% tool \[[^\]]*\]\(([^)]*)\)\s*%}/)
-      end
-
-      if page_obj['workflows']
-        page_obj['workflows'].each{|wf|
-          wf_path = "#{folder}/workflows/#{wf['workflow']}"
-
-          wf_data = JSON.parse(File.open(wf_path).read)
-          page_obj['tools'] += self.extract_workflow_tool_list(wf_data)
-        }
-      end
-      page_obj['tools'] = page_obj['tools'].flatten.sort.uniq
-
-
-      page_obj['tours'] = resources.include?('tours')
-      page_obj['video'] = slide_has_video
-      page_obj['translations'] = Hash.new
-      page_obj['translations']["tutorial"] = tutorial_translations
-      page_obj['translations']["slides"] = slide_translations
-      page_obj['translations']['video'] = slide_has_video # Just demand it?
-      # I feel less certain about this override, but it works well enough in
-      # practice, and I did not find any examples of `type: <anything other
-      # than tutorial>` in topics/*/tutorials/*/tutorial.md but that doesn't
-      # make it future proof.
-      page_obj['type'] = 'tutorial'
-
-      if page_obj.has_key?("enable") and !page_obj['enable'] then
-        if ! page_obj.has_key? 'tags'
-          page_obj['tags'] = Array.new
-        end
-        page_obj['tags'].push('work-in-progress')
-      end
-
-      # Push onto our stack.
-      resource_pages.push(page_obj)
+    slide_has_video = false
+    slide_translations = []
+    if slides.length > 0 then
+      page = slides.sort{|a, b| a[1].path <=> b[1].path}[0][1]
+      slide_has_video = page.data.fetch('video', false)
+      slide_translations = page.data.fetch('translations', [])
     end
 
+    # No matter if there were slides, we override with tutorials if present.
+    tutorial_translations = []
+    if tutorials.length > 0 then
+      page = tutorials.sort{|a, b| a[1].path <=> b[1].path}[0][1]
+      tutorial_translations = page.data.fetch('translations', [])
+    end
+
+    if page.nil? then
+      puts "[GTN/TopicFilter] Could not process material"
+      require 'pp'
+      pp material
+      return {}
+    end
+
+    # Otherwise clone the metadata from it which works well enough.
+    page_obj = page.data.dup
+
+    # Sometimes `hands_on` is set to something like `external`, in which
+    # case it is important to not override it. So we only do that if the
+    # key isn't already set. Then we choose to set it to a test for the
+    # tutorial being present. We probably don't need to test both, but it
+    # is hard to follow which keys are which and safer to test for both in
+    # case someone edits the code later. If either of these exist, we can
+    # automatically set `hands_on: true`
+    if not page_obj.has_key?("hands_on") then
+      page_obj['hands_on'] = tutorials.length > 0
+    end
+
+    # Same for slides, if there's a resource by that name, we can
+    # automatically set `slides: true`
+    if not page_obj.has_key?("slides") then
+      page_obj['slides'] = slides.length > 0
+    end
+
+    folder = material["dir"]
+
+    ymls = Dir.glob("#{folder}/quiz/*.yml") + Dir.glob("#{folder}/quiz/*.yaml")
+    if ymls.length > 0
+      quizzes = ymls.map{ |a| a.split('/')[-1] }
+      page_obj['quiz'] = quizzes.map{|q|
+        quiz_data = YAML.load_file("#{folder}/quiz/#{q}")
+        {
+          "id" => q,
+          "path" => "#{folder}/quiz/#{q}",
+          "title" => quiz_data['title'],
+          "contributors" => quiz_data['contributors'],
+        }
+      }
+    end
+
+    # Similar as above.
+    workflows = Dir.glob("#{folder}/workflwos/*.ga") # TODO: support gxformat2
+    if workflows.length > 0
+      workflow_names = workflows.map{ |a| a.split('/')[-1] }
+      page_obj['workflows'] = workflow_names.map{|wf|
+        x = {
+          "workflow" => wf,
+          "tests" => Dir.glob("#{folder}/workflows/" + wf.gsub(/.ga/, '-test*')).length > 0,
+        }
+        x
+      }
+    end
+
+    # Tool List
+    #
+    # This is exposed in the GTN API to help admins/devs easily get the tool
+    # list for installation.
+    page_obj['tools'] = []
+    if page_obj['hands_on']
+      page_obj['tools'] += page.content.scan(/{% tool \[[^\]]*\]\(([^)]*)\)\s*%}/)
+    end
+
+    if page_obj['workflows']
+      page_obj['workflows'].each{|wf|
+        wf_path = "#{folder}/workflows/#{wf['workflow']}"
+
+        wf_data = JSON.parse(File.open(wf_path).read)
+        page_obj['tools'] += self.extract_workflow_tool_list(wf_data)
+      }
+    end
+    page_obj['tools'] = page_obj['tools'].flatten.sort.uniq
+
+    page_obj['tours'] = tours.length > 0
+    page_obj['video'] = slide_has_video
+    page_obj['translations'] = Hash.new
+    page_obj['translations']["tutorial"] = tutorial_translations
+    page_obj['translations']["slides"] = slide_translations
+    page_obj['translations']['video'] = slide_has_video # Just demand it?
+    # I feel less certain about this override, but it works well enough in
+    # practice, and I did not find any examples of `type: <anything other
+    # than tutorial>` in topics/*/tutorials/*/tutorial.md but that doesn't
+    # make it future proof.
+    page_obj['type'] = 'tutorial'
+
+    if page_obj.has_key?("enable") and !page_obj['enable'] then
+      if ! page_obj.has_key? 'tags'
+        page_obj['tags'] = Array.new
+      end
+      page_obj['tags'].push('work-in-progress')
+    end
+
+    page_obj
+  end
+
+  def self.process_pages(site, pages)
+    if site.data.has_key?('cache_processed_pages') then 
+      return site.data['cache_processed_pages']
+    end
+
+    materials = self.collate_materials(pages).map{|k, v| self.resolve_material(v) }
+    puts "[GTN/TopicFilter] Filling Materials Cache"
+    site.data['cache_processed_pages'] = materials
+
+    materials
+  end
+
+  def self.run_topic_filter(site, pages, topic_name)
+    # Here we make a (cached) call to load materials into memory and sort them
+    # properly.
+    materials = self.process_pages(site, pages)
+
+    # Select out the materials by topic:
+    resource_pages = materials.select{|x| x['topic_name'] == topic_name || x.fetch('tags', []).include?(topic_name)}
+
     # The complete resources we'll return is the introduction slides first
-    # (regardless of alphabetisation), and then the rest of the pages.
-    resource_pages = resource_intro + resource_pages.sort_by{ |k| k.fetch('title', '').downcase}
+    # (EDIT: not anymore, we rely on prioritisation!)
+    # and then the rest of the pages.
+    resource_pages = resource_pages.sort_by{ |k| k.fetch('title', '').downcase}
 
     if resource_pages.length == 0 then
       puts "Error? Could not find any relevant pages for #{topic_name}"
     end
 
-    # Apparently return is optional?
     resource_pages
   end
 end
