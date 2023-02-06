e147ce0b8fc0970afe9ebd886e38f4edd156e41b
hexylena <hexylena@users.noreply.github.com>
2023-02-06 12:04:03 +0000

    deploy: 34e83fef5849d18c0e977cff7beb952ca1471cff

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,30 @@
+function getThemePreference(){
+	params = (new URL(document.location)).searchParams;
+	paramTheme = params.get('theme')
+
+	if(paramTheme){
+		setThemePreference(paramTheme);
+	}
+	return localStorage.getItem('training-theme');
+}
+
+function setThemePreference(newValue) {
+	localStorage.setItem('training-theme', newValue);
+}
+
+
+function setTheme(theme){
+	var old_classes = $("body").attr("class");
+	if (old_classes === undefined){
+		old_classes = "";
+	}
+	var new_classes = old_classes.split(' ').filter(function(x){ return x.substring(0, 6) !== "theme-" })
+	new_classes.push('theme-' + theme)
+
+	$("body").attr('class', new_classes.join(' '))
+	setThemePreference(theme);
+}
+
 (function (window, document) {
 	function onDocumentReady(fn) {
 		if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
@@ -8,38 +35,8 @@
 	}
 
 	onDocumentReady(function () {
-		function getThemePreference(){
-			params = (new URL(document.location)).searchParams;
-			paramTheme = params.get('theme')
-
-			if(paramTheme){
-				setThemePreference(paramTheme);
-			}
-			return localStorage.getItem('training-theme');
-		}
-
-		function setThemePreference(newValue) {
-			localStorage.setItem('training-theme', newValue);
-		}
-
-
-		function setTheme(theme){
-			var old_classes = $("body").attr("class");
-			if (old_classes === undefined){
-				old_classes = "";
-			}
-			var new_classes = old_classes.split(' ').filter(function(x){ return x.substring(0, 6) !== "theme-" })
-
-			if (theme !== "default"){
-				new_classes.push('theme-' + theme)
-			}
-
-			$("body").attr('class', new_classes.join(' '))
-			setThemePreference(theme);
-		}
-
-		$("#theme-selector").click(function(evt){
-			var theme = $(evt.target).data('value');
+		$("#theme-selector button").click(function(evt){
+			var theme = $(evt.target).data('theme');
 			setTheme(theme);
 			if(theme === "straya"){
 				$("body").addClass('downunder');
@@ -53,6 +50,11 @@
 		if(training_theme_cookie){
 			setTheme(training_theme_cookie);
 		}
+
+                // Just for february
+                if(getThemePreference() === null || getThemePreference() === "undefined" || getThemePreference() === undefined){
+                        setTheme("blm");
+                }
+
 	});
 })(window, document);
-
