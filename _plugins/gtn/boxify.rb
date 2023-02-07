86860fc16446f3acfafb771c54dd962ab54d0bd0
Helena Rasche <hexylena@galaxians.org>
2023-02-07 11:33:20 +0100

    Make match non-greedy
    As it operates in an environment where multiple snippet boxes may be squished together

--- _plugins/gtn/boxify.rb
+++ _plugins/gtn/boxify.rb
@@ -185,7 +185,7 @@ module Gtn
       #
       # We're going to rely on never having two on one line
       text.split("\n").map{|line|
-        line.gsub(/<(?<type>[a-z-]*)-title>(?<title>.*)<\/[a-z-]*-title>/){|m|
+        line.gsub(/<(?<type>[a-z-]*)-title>(?<title>.*?)<\/[a-z-]*-title>/){|m|
           title = Regexp.last_match[:title]
           type = Regexp.last_match[:type]
           _, box = self.generate_title(type, title, lang=lang, key)
