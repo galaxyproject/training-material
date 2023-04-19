b8f89083473df63c798257828ee2509378b4f6d4
Helena Rasche <hexylena@galaxians.org>
2023-04-19 12:27:40 +0200

    Add git info to the config endpoint

--- _plugins/jekyll-environment_variables.rb
+++ _plugins/jekyll-environment_variables.rb
@@ -5,6 +5,8 @@ require 'citeproc/ruby'
 require 'csl/styles'
 require 'time'
 require './_plugins/gtn/scholar'
+require './_plugins/gtn/git'
+require './_plugins/gtn'
 
 module Jekyll
 
@@ -20,31 +22,9 @@ module Jekyll
     #  - `site.config['age']` - the age of the site in years
     def generate(site)
 
-      begin
-        git_head = File.open(File.join('.git', 'HEAD')).read.strip.split(' ')[1]
-        git_head_ref = File.open(File.join('.git', git_head)).read.strip
-      rescue
-        git_head_ref = 'none'
-      end
-      site.config['git_revision'] = git_head_ref
-      site.config['gtn_fork'] = ENV['GTN_FORK']
-      begin
-        tags = `git tag -l`.strip.split.sort
-        site.config['git_tags'] = tags
-        site.config['git_tags_recent'] = tags.reverse[0..2]
-      rescue
-        site.config['git_tags'] = []
-        site.config['git_tags_recent'] = []
-      end
-
       # Add other environment variables to `site.config` here...
       Gtn::Scholar.load_bib(site)
-
-      # Get site age.
-      first_commit = Date.parse("2015-06-29")
-      today = Date.today()
-
-      site.config['age'] = (today - first_commit).to_f / 365
+      site.config.update(Gtn::Git.discover)
     end
   end
 end
