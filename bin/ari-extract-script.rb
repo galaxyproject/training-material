bd978e1eef2ef94679ee0e37af3d1519a8de5fb6
Helena Rasche <hexylena@galaxians.org>
2023-05-16 12:02:36 +0200

    document voices, add a couple more

--- bin/ari-extract-script.rb
+++ bin/ari-extract-script.rb
@@ -35,6 +35,8 @@ APPROVED_VOICES = {
     { 'id' => 'Olivia', 'lang' => 'en-AU', 'neural' => true },
     { 'id' => 'Raveena', 'lang' => 'en-IN', 'neural' => false },
     { 'id' => 'Salli', 'lang' => 'en-US', 'neural' => true }
+    { 'id' => 'Ayanda', 'lang' => 'en-ZA', 'neural' => true }
+    { 'id' => 'Geraint', 'lang' => 'en-GB-WLS', 'neural' => false }
   ],
   'es' => [
     { 'id' => 'Miguel', 'lang' => 'es-US', 'neural' => false },
--- bin/ari-gen-sample-audio.sh
+++ bin/ari-gen-sample-audio.sh
@@ -2,15 +2,17 @@ sample_sentence='Welcome to the Galaxy Training Network! We are a platform for F
 sample=$(mktemp)
 echo "$sample_sentence" > "$sample"
 
-ruby bin/ari-synthesize.rb --aws --voice Amy --lang en-GB  --file $sample -o amy.mp3
-ruby bin/ari-synthesize.rb --aws --voice Aria --lang en-NZ  --file $sample -o aria.mp3
-ruby bin/ari-synthesize.rb --aws --voice Brian --lang en-GB  --file $sample -o brian.mp3
-ruby bin/ari-synthesize.rb --aws --voice Emma --lang en-GB  --file $sample -o emma.mp3
-ruby bin/ari-synthesize.rb --aws --voice Joanna --lang en-US  --file $sample -o joanna.mp3
-ruby bin/ari-synthesize.rb --aws --voice Joey --lang en-US  --file $sample -o joey.mp3
-ruby bin/ari-synthesize.rb --aws --voice Kendra --lang en-US  --file $sample -o kendra.mp3
-ruby bin/ari-synthesize.rb --aws --voice Matthew --lang en-US  --file $sample -o matthew.mp3
-ruby bin/ari-synthesize.rb --aws --voice Nicole --lang en-AU --non-neural --file $sample -o nicole.mp3
-ruby bin/ari-synthesize.rb --aws --voice Olivia --lang en-AU  --file $sample -o olivia.mp3
-ruby bin/ari-synthesize.rb --aws --voice Raveena --lang en-IN --non-neural --file $sample -o raveena.mp3
-ruby bin/ari-synthesize.rb --aws --voice Salli --lang en-US  --file $sample -o salli.mp3
+ruby bin/ari-synthesize.rb --aws --voice Amy --lang en-GB  --file $sample -o assets/audio/amy.mp3
+ruby bin/ari-synthesize.rb --aws --voice Aria --lang en-NZ  --file $sample -o assets/audio/aria.mp3
+ruby bin/ari-synthesize.rb --aws --voice Brian --lang en-GB  --file $sample -o assets/audio/brian.mp3
+ruby bin/ari-synthesize.rb --aws --voice Emma --lang en-GB  --file $sample -o assets/audio/emma.mp3
+ruby bin/ari-synthesize.rb --aws --voice Joanna --lang en-US  --file $sample -o assets/audio/joanna.mp3
+ruby bin/ari-synthesize.rb --aws --voice Joey --lang en-US  --file $sample -o assets/audio/joey.mp3
+ruby bin/ari-synthesize.rb --aws --voice Kendra --lang en-US  --file $sample -o assets/audio/kendra.mp3
+ruby bin/ari-synthesize.rb --aws --voice Matthew --lang en-US  --file $sample -o assets/audio/matthew.mp3
+ruby bin/ari-synthesize.rb --aws --voice Nicole --lang en-AU --non-neural --file $sample -o assets/audio/nicole.mp3
+ruby bin/ari-synthesize.rb --aws --voice Olivia --lang en-AU  --file $sample -o assets/audio/olivia.mp3
+ruby bin/ari-synthesize.rb --aws --voice Raveena --lang en-IN --non-neural --file $sample -o assets/audio/raveena.mp3
+ruby bin/ari-synthesize.rb --aws --voice Salli --lang en-US  --file $sample -o assets/audio/salli.mp3
+ruby bin/ari-synthesize.rb --aws --voice Ayanda --lang en-ZA --file $sample -o assets/audio/ayanda.mp3
+ruby bin/ari-synthesize.rb --aws --voice Geraint --lang en-GB-WLS --non-neural --file $sample -o assets/audio/geraint.mp3
