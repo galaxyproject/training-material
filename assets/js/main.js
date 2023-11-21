00d6e90cbc97db42de431ce9f938ae50637e1a1b
Helena Rasche <hexylena@galaxians.org>
2023-11-21 14:33:37 +0100

    support citing quotes

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,3 +195,10 @@ document.querySelectorAll('div.highlight').forEach((snippet) => {
 var clipboardSnippets=new ClipboardJS('[data-clipboard-snippet]',{
     target:function(trigger){return trigger.nextElementSibling;
 }});
+
+// Cited blockquotes
+document.querySelectorAll("blockquote[cite]").forEach(bq => {
+	var url = bq.getAttribute("cite");
+	var author = "— " + bq.getAttribute("author") + " " || "";
+	bq.insertAdjacentHTML("beforeend", `<footer>${author}<cite class="text-muted"><a href="${url}"><i>Source</i></a></cite></footer>`)
+})
