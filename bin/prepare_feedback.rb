2610652c890b8e574a2b390ccd092d1077098aa0
Helena Rasche <hexylena@galaxians.org>
2024-02-19 14:58:52 +0100

    update csv output

--- bin/prepare_feedback.rb
+++ bin/prepare_feedback.rb
@@ -4,8 +4,6 @@
 require 'yaml'
 require 'csv'
 
-
-
 TEST_PHRASES = [
   'TESTING',
   'test',
@@ -13,7 +11,7 @@ TEST_PHRASES = [
 ]
 
 TITLE_TOPIC = /^(?<tutorial>.*) \((?<topic>.*)\)$/
-TUTO_ID = /^(?<tutorial>[A-Za-z0-9_-]*)\/(?<topic>[A-Za-z0-9_-]*):?(?<lang>[a-zA-Z][a-zA-Z])?$/
+TUTO_ID = %r{^(?<topic>[A-Za-z0-9_-]*)/(?<tutorial>[A-Za-z0-9_-]*):?(?<lang>[a-zA-Z][a-zA-Z])?$}
 
 data = CSV.parse($stdin.read, col_sep: "\t", headers: true, quote_char: '|')
 data = data.map do |x|
@@ -21,7 +19,6 @@ data = data.map do |x|
   x[:rating] = x['How much did you like this tutorial?']
   x[:pro] = x['What did you like?']
   x[:con] = x['What could be improved?']
-  x[:tutorial_topic] = x['Tutorial']
   x[:id] = x['Temporary Tutorial ID']
 
   x.delete('Timestamp')
@@ -33,14 +30,43 @@ data = data.map do |x|
   x
 end
 
+def lookup_tuto(topic_id, tuto_id)
+  @cache ||= {}
+  @cache.fetch("#{topic_id}/#{tuto_id}") do |key|
+    @cache[key] = nil
+
+    file = "topics/#{topic_id}/tutorials/#{tuto_id}/tutorial.md"
+    if File.exist? file
+      data = YAML.load_file(file)
+      @cache[key] = data['title']
+    else
+      file = "topics/#{topic_id}/tutorials/#{tuto_id}/slides.html"
+      if File.exist? file
+        data = YAML.load_file(file)
+        @cache[key] = data['title']
+      else
+        puts "No file for #{topic_id}/#{tuto_id}"
+      end
+    end
+  end
+end
+
+def lookup_topic(topic_id)
+  @cache ||= {}
+  @cache.fetch(topic_id) do |key|
+    file = "metadata/#{topic_id}.yaml"
+    return nil unless File.exist? file
+
+    data = YAML.load_file(file)
+    @cache[key] = data['title']
+  end
+end
+
 data = data.reject { |x| x[:timestamp].nil? }
            # remove rows with NaN on note, pro and con
            .reject { |x| (x[:rating].nil? && x[:pro].nil? && x[:con].nil?) }
            # Ensure that they properly have a tutorial name and title
-           # If this returns NIL, then we'll ignore those.
-           # .select { |x| TITLE_TOPIC.match(x[:tutorial_topic]) }
            .reject { |x| x[:id].nil? or x[:id].empty? }
-  # .reject{ |x| x[:id] =~ / / }
            .select { |x| TUTO_ID.match(x[:id]) }
 
 # Mutate
@@ -53,39 +79,38 @@ data.each do |x| # Various cleanups/extractions
   x[:month] = DateTime.parse(x[:timestamp]).strftime('%Y-%m')
 
   # Extract tutorial/topic
-  m = TITLE_TOPIC.match(x[:tutorial_topic])
-  x.delete('tutorial_topic')
   mq = TUTO_ID.match(x[:id])
   x[:lang] = mq[:lang].downcase if mq[:lang]
-  x[:topic] = mq[:topic]
-  x[:tutorial] = mq[:tutorial]
+  x[:topic_id] = mq[:topic]
+  x[:tutorial_id] = mq[:tutorial]
+
+  x[:topic] = lookup_topic(x[:topic_id])
+  x[:tutorial] = lookup_tuto(x[:topic_id], x[:tutorial_id])
+  # p "Lookup #{x[:topic_id]}/#{x[:tutorial_id]} => #{x[:tutorial]} (#{x[:topic]})"
 
   # Replace N/As with nil
   x[:pro] = x[:pro] == 'N/A' || x[:pro] == 'NA' ? nil : x[:pro]
   x[:con] = x[:con] == 'N/A' || x[:con] == 'NA' ? nil : x[:con]
-
 end
-  .select do |x|
-    !TEST_PHRASES.include? x[:pro] and !TEST_PHRASES.include? x[:con] and !x[:pro].to_s.include? 'Helena Testing'
-  end
+    .select do |x|
+  !TEST_PHRASES.include? x[:pro] and !TEST_PHRASES.include? x[:con] and !x[:pro].to_s.include? 'Helena Testing'
+end
 
 CSV.open('metadata/feedback.csv', 'wb') do |csv|
-  csv << [nil, 'note', 'pro', 'con', 'anonymous', 'tutorial', 'topic', 'month', 'date', 'lang']
+  csv << [nil, 'note', 'pro', 'con', 'anonymous', 'tutorial', 'topic', 'month', 'date']
   data.each.with_index do |row, index|
-    csv << [index, row[:rating], row[:pro], row[:con], nil, row[:tutorial], row[:topic], row[:month], row[:date], row[:lang]]
+    csv << [index, row[:rating], row[:pro], row[:con], nil, row[:tutorial], row[:topic], row[:month], row[:date]]
   end
 end
 
 # Also save as yaml
-File.open('metadata/feedback2.yaml', 'w') do |file|
-  file.write(
-    data.group_by { |x| x[:tutorial] }.map do |tutorial, feedback|
-      fg = feedback.group_by { |x| x[:topic] }
-        .map do |topic, feedback|
-          [topic, feedback.map { |x| {"rating" => x[:rating], "pro" => x[:pro], "con" => x[:con], "date" => x[:date], "lang" => x[:lang]} }]
-        end
-      [tutorial, fg.to_h]
-    end.to_h.to_yaml
-  )
-end
-
+yaml_data = data.group_by { |x| x[:topic_id] }.to_h do |topic, feedback|
+  fg = feedback.group_by { |x| x[:tutorial_id] }
+               .map do |tutorial, feedback|
+    [tutorial, feedback.map do |x|
+                 { 'rating' => x[:rating], 'pro' => x[:pro], 'con' => x[:con], 'date' => x[:date], 'lang' => x[:lang] }
+               end]
+  end
+  [topic, fg.to_h]
+end.to_yaml
+File.write('metadata/feedback2.yaml', yaml_data)
