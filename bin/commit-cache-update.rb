899715bc73125058b691c6f56315a4cd5eb9102c
Helena Rasche <hexylena@galaxians.org>
2024-03-04 10:22:37 +0100

    fix commit cache metadata

--- bin/commit-cache-update.rb
+++ bin/commit-cache-update.rb
@@ -1,11 +1,6 @@
-require './_plugins/gtn'
+require 'jekyll'
+require './_plugins/gtn/mod.rb'
 
 # Write the commit log
 Gtn::ModificationTimes.generate_cache
 Gtn::PublicationTimes.generate_cache
-
-# Gziped?
-# require 'zlib'
-# File.write("metadata/git-mod-#{rev}.txt.gz", Zlib.gzip(Gtn::ModificationTimes.command))
-# File.write("metadata/git-pub-#{rev}.txt.gz", Zlib.gzip(Gtn::PublicationTimes.command))
-# p Gtn::ModificationTimes.discover_caches
