40eb4f545b9b92d2f3eef6c7194a9f6fe6226605
Saskia Hiltemann <zazkia@gmail.com>
2024-04-10 11:26:44 +0200

    undo hacky mathjax solution

--- _plugins/jekyll-mathjax.rb
+++ _plugins/jekyll-mathjax.rb
@@ -5,11 +5,4 @@ Jekyll::Hooks.register :pages, :post_init do |page|
     'mermaid' => page.content =~ /```mermaid/ || page.content =~ /pre class="mermaid"/ || page.data['layout'] == 'workflow-list',
   }
 
-  # simplify math expressions; automatically replace
-  # $$ with \\( and \\) to support inline math with $$
-  if page.content
-    page.content = page.content.gsub(/\$\$(.*?)\$\$/) {|m| m.gsub('_','\\\\_')}
-    page.content = page.content.gsub(/\$\$(.*?)\$\$/,'\\\\\\\\( \1 \\\\\\\\)')
-  end
-
 end
