a8ca36b26fa2d443be3cea6d50a047a1f8293d78
Helena Rasche <hexylena@galaxians.org>
2024-04-10 12:25:06 +0200

    add to post-build checks

--- bin/check-indent.rb
+++ bin/check-indent.rb
@@ -6,6 +6,7 @@ def check_indent(file)
   doc = Nokogiri::HTML(File.open(file))
   # Find all <pre> tags
   # <div class="language-plaintext highlighter-rouge"><div class="highlight"><pre class="highlight"><code>
+  ec = false
   doc.css('div.language-plaintext.highlighter-rouge div.highlight pre.highlight code').each do |pre|
     # Get the text content of the <pre> tag
     content = pre.text
@@ -18,16 +19,25 @@ def check_indent(file)
       lines.each do |line|
         if line =~ /^\s+/
           puts "#{file}: Indentation error: #{line}"
+          ec = true
         end
       end
     end
   end
+  ec
 end
 
+should_exit = false
 Find.find('./_site/training-material/topics/') do |path|
   if path =~ /tutorial.*\.html$/
-    check_indent(path)
+    if check_indent(path)
+      should_exit = true
+    end
   end
+end
+
+if should_exit
+  exit 1
 end
 #
 # check_indent(ARGV[0])
