d75b68edcb6cf5daf26978db4eae7eb951a02e8d
Saskia Hiltemann <zazkia@gmail.com>
2024-04-10 13:44:04 +0200

    fix math rendering

--- _plugins/jekyll-mathjax.rb
+++ _plugins/jekyll-mathjax.rb
@@ -5,4 +5,11 @@ Jekyll::Hooks.register :pages, :post_init do |page|
     'mermaid' => page.content =~ /```mermaid/ || page.content =~ /pre class="mermaid"/ || page.data['layout'] == 'workflow-list',
   }
 
+  # some fixes for mathjax: escape underscores
+  # both in inline mode \\( .. \\) and block mode $$ ..$$
+  if page.content
+    page.content = page.content.gsub(/\$\$(.*?)\$\$/) {|m| m.gsub('_','\\\\_')}
+    page.content = page.content.gsub(/\\\\\((.*?)\\\\\)/) {|m| m.gsub('_','\\\\_')}
+
+  end
 end
