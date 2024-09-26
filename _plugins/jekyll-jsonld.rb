db87d81ed5f3e69870f2090102bfeb55b95009d8
Helena <hexylena@galaxians.org>
2024-09-26 17:20:47 +0200

    add rstudio

--- _plugins/jekyll-jsonld.rb
+++ _plugins/jekyll-jsonld.rb
@@ -891,6 +891,11 @@ module Jekyll
                           url: url,
                           name: "Quarto/RMarkdown Notebook"
                         })
+          mentions.push({
+                          '@type': 'Thing',
+                          url: "https://bio.tools/tool/rstudio",
+                          name: "RStudio"
+                        })
         end
       end
 
