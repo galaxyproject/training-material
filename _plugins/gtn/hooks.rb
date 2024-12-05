833143dc147a4d2cd898a3e3e18c0a3a7a7d5cb2
Helena Rasche <hexylena@galaxians.org>
2024-12-05 16:25:53 +0100

    ugly workaround

--- _plugins/gtn/hooks.rb
+++ _plugins/gtn/hooks.rb
@@ -6,13 +6,18 @@ module Gtn
   # Parse the git repo to get some facts
   module Hooks
 
+    ##
+    # Generate the by-tool pages
+    # Params:
+    # +site+:: Jekyll site object
     def self.by_tool(site)
       Jekyll.logger.info "[GTN/Hooks/by_tool] Started"
-      TopicFilter.list_materials_by_tool(site).each do |tool, tutorials|
-        page2 = Jekyll::PageWithoutAFile.new(site, '', 'by-tool/', "#{tool.gsub('%20', ' ')}.html")
-        page2.content = nil
-        page2.data['layout'] = 'by_tool'
-        page2.data['short_tool'] = tool
+
+      tools = TopicFilter.list_materials_by_tool(site)
+      tools.reject!{|tool, _| tool.include?('{{')}
+
+      tools.each do |tool, tutorials|
+        # tool: e.g. `saskia-hiltemann/krona_text/krona-text`
 
         ordered_tool_ids = tutorials['tool_id']
           .map{|x| 
@@ -27,20 +32,38 @@ module Gtn
           .map{|x| [x[0], x[1], Gem::Version.new(fix_version(x[1]))]}
           .sort_by{|x| x[2]}
 
-        page2.data['observed_tool_ids'] = ordered_tool_ids.map{|x| x[0..1]}.reverse
-        page2.data['tutorial_list'] = tutorials['tutorials']
-        page2.data['latest_tool_id'] = ordered_tool_ids.map{|x| x[0]}.last
-
         # Redirect from the older, shorter IDs that have more potential for conflicts.
         if tool.include?('/')
           previous_id = tool.split('/')[0] + '/' + tool.split('/')[2]
         else
           previous_id = tool # No change
         end
-        page2.data['redirect_from'] = ["/by-tool/#{previous_id.gsub('%20', ' ')}"]
-        site.pages << page2
-      end
 
+        page2 = Jekyll::PageWithoutAFile.new(site, '', 'by-tool/', "#{tool.gsub('%20', ' ')}.html")
+        page2.content = nil
+        page2.data['layout'] = 'by_tool'
+        page2.data['short_tool'] = tool
+        page2.data['observed_tool_ids'] = ordered_tool_ids.map{|x| x[0..1]}.reverse
+        page2.data['tutorial_list'] = tutorials['tutorials']
+        page2.data['latest_tool_id'] = ordered_tool_ids.map{|x| x[0]}.last
+        # page2.data['redirect_from'] = ["/by-tool/#{previous_id.gsub('%20', ' ')}"]
+        site.pages << page2
+
+        # TODO: For whatever reason the redirect_from does NOT work, even this
+        # early in the hooks, so we're just going to write the file and call it
+        # a day. Someone should fix this someday. My apologies for leaving it like this.
+        if previous_id != tool
+          page2 = Jekyll::PageWithoutAFile.new(site, '', 'by-tool/', "#{previous_id}.html")
+          page2.content = nil
+          page2.data['layout'] = 'by_tool'
+          page2.data['short_tool'] = tool
+          page2.data['observed_tool_ids'] = ordered_tool_ids.map{|x| x[0..1]}.reverse
+          page2.data['tutorial_list'] = tutorials['tutorials']
+          page2.data['latest_tool_id'] = ordered_tool_ids.map{|x| x[0]}.last
+          site.pages << page2
+        end
+
+      end
     end
   end
 end
