9f8daaca3c0591e7c8c13b471f5a6b1902636cf0
Saskia Hiltemann <saskia.hiltemann@protonmail.com>
2026-09-23 12:50:05 +0200

    keep manual list of Galaxies to check for compatibility

--- bin/supported-fetch.py
+++ bin/supported-fetch.py
@@ -7,6 +7,7 @@ import logging
 import multiprocessing.pool
 import os
 import requests
+import yaml
 
 
 def fetch_and_extract_individual_server_tools(server):
@@ -53,27 +54,19 @@ def extract_public_galaxy_servers_tools():
     server_tools = {}
 
     to_process = []
-    serverlist = requests.get('https://galaxyproject.org/use/feed.json').json()
-    for server in serverlist:
-        # We intentionally drop all usegalaxy.eu subdomains. They're all the
-        # same as the top level domain and just pollute the supported instances
-        # list.
-        if '.usegalaxy.eu' in server['url']:
-            continue
-        # Apparently the french do it too
-        if '.usegalaxy.fr' in server['url']:
-            continue
-        # The aussies will soon
-        if '.usegalaxy.org.au' in server['url']:
-            continue
-        # No test servers permitted
-        if 'test.' in server['url']:
-            continue
+    #serverlist = requests.get('https://galaxyproject.org/use/feed.json').json()
 
-        s = { 'name': server['title'], 'url': server['url'] }
-        to_process.append(s)
+    with open("metadata/galaxy-servers.yaml") as f:
+      try:
+        serverlist = yaml.safe_load(f)
+      except yaml.YAMLError as exc:
+        print(exc)
 
-    pool = multiprocessing.pool.ThreadPool(processes=20)
+    for server in serverlist['galaxies']:
+      s = { 'name': server['name'], 'url': server['url'] }
+      to_process.append(s)
+
+    pool = multiprocessing.pool.ThreadPool(processes=200)
     processed = pool.map(fetch_and_extract_individual_server_tools, to_process, chunksize=1)
     pool.close()
 
